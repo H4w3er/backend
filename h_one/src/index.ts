c8afc14d873b2416c64eff1d16fa95b8f76c5abc
H4w3er <mr.gavrik378@list.ru>
2024-10-15 21:52:38 +0300

    added 3 endpoints

--- h_one/src/index.ts
+++ h_one/src/index.ts
@@ -1,11 +1,47 @@
-const express = require('express')
+import express, {Request, Response} from 'express'
+
 const app = express()
 const port = 3000
+let videos = [
+    {id: 1, title: 'first', author: 'n1'},
+    {id: 2, title: 'sec', author: 'n2'},
+    {id: 3, title: 'therd', author: 'n3'}
+]
 
-app.get('/', (req, res) => {
-    res.send('Hello')
+app.get('/videos/:id', (req, res) => {
+    const foundVideo = videos.find((c => c.id === +req.params.id))
+    if (!foundVideo) {
+        res.sendStatus(404);
+        return;
+    } else res.json(foundVideo)
 })
-
+app.get('/videos', (req, res) => {
+    if (req.query.title) { const foundVideos = videos
+        .filter(c => c.title.indexOf(req.query.title as string) > -1)
+        res.json(foundVideos)
+    } else res.json(videos)
+})
+app.post('/videos', (req, res) => {
+    const newVideo = {
+        id: +(Date.now()),
+        title: req.query.title as string,
+        author: 'biba'
+    }
+    videos.push(newVideo)
+    res.json(newVideo)
+})
 app.listen (port, ()=>{
     console.log(`Example listening on port ${port}`)
 })
+
+
+/*export const video1: any /!*VideoDBType*!/ = {
+    id: Date.now() + Math.random(),
+    title: 't' + Date.now() + Math.random(),
+    // author: 'a' + Date.now() + Math.random(),
+    // canBeDownloaded: true,
+    // minAgeRestriction: null,
+    // createdAt: new Date().toISOString(),
+    // publicationDate: new Date().toISOString(),
+    // availableResolution: [Resolutions.P240],
+}*/
