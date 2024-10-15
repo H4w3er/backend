3f88c0b0a4f117de332318be1cd33b41f7d906ea
H4w3er <mr.gavrik378@list.ru>
2024-10-15 19:15:23 +0300

    added ignored files

--- h_one/index.js
+++ h_one/index.js
@@ -1,1 +1,11 @@
-console.log('Happy developing ✨')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen (port, ()=>{
+    console.log(`Example listening on port ${port}`)
+})
