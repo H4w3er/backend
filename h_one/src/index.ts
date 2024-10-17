ce322e32a38cfd921afc44e5aadfa7053ee0a85d
H4w3er <mr.gavrik378@list.ru>
2024-10-17 21:21:00 +0300

    updated index.ts

--- h_one/src/index.ts
+++ h_one/src/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express'
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 const avResolution = ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]
 
 const jsonBodyMiddleware = express.json();
