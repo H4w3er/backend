f944cbbcb2555548a37f0bb93b44f2f58ed73658
H4w3er <mr.gavrik378@list.ru>
2024-10-25 19:37:23 +0300

    okay, now should work

--- h_two/src/routers/posts-router.ts
+++ h_two/src/routers/posts-router.ts
@@ -7,7 +7,7 @@ import {authMiddleware} from "../middlewares/authMiddleware";
 
 const titleValidation = body('title').trim().isLength({min:1, max:30}).withMessage("Title should be more than 1 and less than 30");
 const shortDescriptionValidation = body('shortDescription').isLength({min:1, max:100}).withMessage("shortDescription should be more than 1 and less than 100");
-const contentValidation = body('content').isLength({min:1, max:1000}).withMessage("Content should be more than 1 and less than 1000")
+const contentValidation = body('content').trim().isLength({min:1, max:1000}).withMessage("Content should be more than 1 and less than 1000")
 const  blogIdValidation= body('blogId').custom(value => {
     return postsRepository.isBlog(value);
 }).withMessage("Blog not found")
