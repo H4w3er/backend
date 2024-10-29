3f353648c7611511a2a008af9941d4c4feb11ed2
H4w3er <mr.gavrik378@list.ru>
2024-10-29 11:59:06 +0300

    added settings for port, routs and mongodb

---
+++ h_two/src/settings.ts
@@ -0,0 +1,12 @@
+import {config} from "dotenv";
+config()
+
+export const SETTINGS = {
+    PORT: process.env.PORT || 3001,
+    PATH: {
+        BLOGS: '/blogs',
+        POSTS: '/posts'
+    },
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://0.0.0.0:27017',
+    DB_NAME: process.env.DB_NAME || ''
+}
