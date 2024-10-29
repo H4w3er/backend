8eac0792b4b40b99b52a79c8b10c7b94c4976930
H4w3er <mr.gavrik378@list.ru>
2024-10-29 11:59:41 +0300

    changed some stuff for settings

--- h_two/src/index.ts
+++ h_two/src/index.ts
@@ -2,16 +2,15 @@ import express, {Request, Response} from 'express'
 import {blogsRouter} from "./routers/blogs-router";
 import {postsRouter} from "./routers/posts-router";
 import {blogsRepository} from "./repositories/blogs-repository";
-//import bodyParser from 'body-parser'
+import {SETTINGS} from "./settings";
 
 const app = express()
-const port = process.env.PORT || 5000;
 
 const jsonBodyMiddleware = express.json();
 app.use(jsonBodyMiddleware)
 
-app.use('/blogs', blogsRouter)
-app.use('/posts', postsRouter)
+app.use(SETTINGS.PATH.BLOGS, blogsRouter)
+app.use(SETTINGS.PATH.POSTS, postsRouter)
 
 app.get('/', (req, res) => {
     res.status(200).json({version: '1.0'});
@@ -20,6 +19,6 @@ app.delete('/testing/all-data', (req, res) => {
     if (blogsRepository.deleteAll()) res.sendStatus(204)
 })
 
-app.listen (port, ()=>{
-    console.log(`Example listening on port ${port}`)
+app.listen (SETTINGS.PORT, ()=>{
+    console.log(`Example listening on port ${SETTINGS.PORT}`)
 })
