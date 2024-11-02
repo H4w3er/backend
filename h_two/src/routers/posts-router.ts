46484272097880974da8ccc9f0ab7572ee84670c
H4w3er <mr.gavrik378@list.ru>
2024-11-02 14:33:09 +0300

    now endpoints async

--- h_two/src/routers/posts-router.ts
+++ h_two/src/routers/posts-router.ts
@@ -8,14 +8,14 @@ import {authMiddleware} from "../middlewares/authMiddleware";
 const titleValidation = body('title').trim().isLength({min:1, max:30}).withMessage("Title should be more than 1 and less than 30");
 const shortDescriptionValidation = body('shortDescription').isLength({min:1, max:100}).withMessage("shortDescription should be more than 1 and less than 100");
 const contentValidation = body('content').trim().isLength({min:1, max:1000}).withMessage("Content should be more than 1 and less than 1000")
-const  blogIdValidation= body('blogId').custom(value => {
-    return postsRepository.isBlog(value);
+const  blogIdValidation= body('blogId').custom(async value => {
+    return await postsRepository.isBlog(value);
 }).withMessage("Blog not found")
 
 export const postsRouter = Router({})
 
-postsRouter.get('/', (req, res) => {
-    let posts = postsRepository.findPosts()
+postsRouter.get('/', async(req, res) => {
+    let posts = await postsRepository.findPosts()
     res.send(posts);
 })
 postsRouter.post('/',
@@ -25,14 +25,14 @@ postsRouter.post('/',
     contentValidation,
     blogIdValidation,
     inputValidationMiddleware,
-    (req, res) => {
-        const newPost = postsRepository.createPost(req.body.title, req.body.shortDescription,
+    async (req, res) => {
+        const newPost = await postsRepository.createPost(req.body.title, req.body.shortDescription,
             req.body.content, req.body.blogId)
         res.status(201).send(newPost)
     })
 
-postsRouter.get('/:id', (req, res) => {
-    let blog = postsRepository.findPostById(req.params.id)
+postsRouter.get('/:id', async (req, res) => {
+    let blog = await postsRepository.findPostById(req.params.id)
     if (blog){
         res.status(200).send(blog)
     } else res.sendStatus(404);
@@ -44,16 +44,16 @@ postsRouter.put('/:id',
     contentValidation,
     blogIdValidation,
     inputValidationMiddleware,
-    (req, res) => {
-        if(postsRepository.updatePost(req.params.id, req.body.title, req.body.shortDescription,
+    async (req, res) => {
+        if(await postsRepository.updatePost(req.params.id, req.body.title, req.body.shortDescription,
             req.body.content, req.body.blogId)) {
             res.sendStatus(204)
         } else res.sendStatus(404)
     })
 
 postsRouter.delete('/:id', authMiddleware,
-    (req, res) => {
-    if (postsRepository.deletePost(req.params.id)){
+    async (req, res) => {
+    if (await postsRepository.deletePost(req.params.id)){
         res.sendStatus(204)
     } else {
         res.sendStatus(404)
