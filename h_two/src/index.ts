cc8decd9f6c15144dcadc2f1f26236cabc40b1f9
H4w3er <mr.gavrik378@list.ru>
2024-11-02 17:58:52 +0300

    updated version

--- h_two/src/index.ts
+++ h_two/src/index.ts
@@ -14,7 +14,7 @@ app.use(SETTINGS.PATH.BLOGS, blogsRouter)
 app.use(SETTINGS.PATH.POSTS, postsRouter)
 
 app.get('/', (req, res) => {
-    res.status(200).json({version: '1.0'});
+    res.status(200).json({version: '2.0'});
 })
 app.delete('/testing/all-data', async (req, res) => {
     if (await blogsRepository.deleteAll()) res.sendStatus(204)
