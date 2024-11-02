dc1c6aa0223dc36e0fa22f4b582f4b5f470f41bb
H4w3er <mr.gavrik378@list.ru>
2024-11-02 14:33:02 +0300

    now endpoints async

--- h_two/src/routers/blogs-router.ts
+++ h_two/src/routers/blogs-router.ts
@@ -13,46 +13,51 @@ const websiteUrlValidation = body('websiteUrl').isLength({min:1, max:100}).withM
     return pattern.test(value);
 }).withMessage("Bad URL")
 
-blogsRouter.get('/', (req, res) => {
-    let blogs = blogsRepository.findBlogs()
-    res.send(blogs);
+blogsRouter.get('/',
+    async (req, res) => {
+    const foundBlog = await blogsRepository.findBlogs()
+    res.send(foundBlog);
 })
+
 blogsRouter.post('/',
     authMiddleware,
     nameValidation,
     descriptionValidation,
     websiteUrlValidation,
     inputValidationMiddleware,
-    (req, res) => {
-    const newBlog = blogsRepository.createBlog(req.body.name, req.body.description,
+    async (req, res) => {
+    const newBlog = await blogsRepository.createBlog(req.body.name, req.body.description,
         req.body.websiteUrl)
     res.status(201).send(newBlog)
 })
-blogsRouter.get('/:id', (req, res) => {
-    let blog = blogsRepository.findBlogsById(req.params.id)
+
+blogsRouter.get('/:id',
+    async (req, res) => {
+    let blog = await blogsRepository.findBlogsById(req.params.id)
     if (blog){
         res.status(200).send(blog)
     } else res.sendStatus(404);
 })
+
 blogsRouter.put('/:id',
     authMiddleware,
     nameValidation,
     descriptionValidation,
     websiteUrlValidation,
     inputValidationMiddleware,
-    (req, res) => {
-    if(blogsRepository.updateBlog(req.params.id, req.body.name, req.body.description,
+    async (req, res) => {
+    if(await blogsRepository.updateBlog(req.params.id, req.body.name, req.body.description,
         req.body.websiteUrl)) {
         res.sendStatus(204)
     } else res.sendStatus(404)
 })
-blogsRouter.delete('/:id', authMiddleware,  (req, res) => {
-    if (blogsRepository.deleteBlog(req.params.id)){
+
+blogsRouter.delete('/:id', authMiddleware,
+    async (req, res) => {
+    if (await blogsRepository.deleteBlog(req.params.id)){
         res.sendStatus(204)
     } else {
         res.sendStatus(404)
     }
 })
-blogsRouter.delete('/testing/all-data', (req, res) => {
-    if (blogsRepository.deleteAll()) res.sendStatus(204)
-})
+
