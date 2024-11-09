b0820c1100b344e46742a7c9a9fc748c500a86ed
H4w3er <mr.gavrik378@list.ru>
2024-11-09 13:12:00 +0300

    added 2 new endpoints

--- h_two/src/routers/blogs-router.ts
+++ h_two/src/routers/blogs-router.ts
@@ -1,17 +1,11 @@
 import {Router} from "express";
 import {blogsService} from "../domain/blogs-service";
-import {body, validationResult} from "express-validator";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 import {authMiddleware} from "../middlewares/authMiddleware";
+import {nameValidation, descriptionValidation, websiteUrlValidation} from "../middlewares/blogs-validation"
+import {contentValidation, shortDescriptionValidation, titleValidation} from "../middlewares/posts-validation";
 
 export const blogsRouter = Router({})
-
-const nameValidation = body('name').trim().isLength({min:1, max:15}).withMessage("Name should be more than 1 and less than 15");
-const descriptionValidation = body('description').isLength({min:1, max:500}).withMessage("Description should be more than 1 and less than 500");
-const websiteUrlValidation = body('websiteUrl').isLength({min:1, max:100}).withMessage("Bad length").custom(value => {
-    const pattern = /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/
-    return pattern.test(value);
-}).withMessage("Bad URL")
 
 blogsRouter.get('/',
     async (req, res) => {
@@ -62,3 +56,22 @@ blogsRouter.delete('/:id', authMiddleware,
     }
 })
 
+blogsRouter.get('/:id/posts', async (req,res) => {
+    // @ts-ignore
+    const foundPosts = await blogsService.postsForBlog(req.params.id, req.query.sortBy, req.query.sortDirection, req.query.pageNumber, req.query.pageSize)
+    if (foundPosts) res.status(200).send(foundPosts)
+    else res.sendStatus(404)
+})
+
+blogsRouter.post('/:id/posts',
+    authMiddleware,
+    titleValidation,
+    shortDescriptionValidation,
+    contentValidation,
+    inputValidationMiddleware,
+    async (req, res) => {
+    let post = await blogsService.createPostForBlog(req.params.id, req.body.title, req.body.shortDescription, req.body.content)
+        if (post){
+            res.status(200).send(post)
+    } else res.sendStatus(404);
+})
