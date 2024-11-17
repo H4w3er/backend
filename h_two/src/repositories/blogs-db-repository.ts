b232fc4ac3b9b824b7fbe141aa2e7fee59f6d5b5
H4w3er <mr.gavrik378@list.ru>
2024-11-17 19:11:34 +0300

    mapper and logic for get endpoints- deleted

--- h_two/src/repositories/blogs-db-repository.ts
+++ h_two/src/repositories/blogs-db-repository.ts
@@ -1,65 +1,12 @@
 import {blogCollection, postCollection} from "../db/mongo-db";
-import {BlogDbType} from "../db/blogs-type-db";
 import {ObjectId} from "mongodb";
 import {postsRepository} from "./posts-db-repository";
 
-const blogMapper = (value: any) => {
-    if (value) {
-    const mappedBlog = {
-        id: value._id,
-        name: value.name,
-        description: value.description,
-        websiteUrl: value.websiteUrl,
-        createdAt: value.createdAt,
-        isMembership: value.isMembership
-    }
-    return mappedBlog;
-    } else return null;
-}
-
-const blogFilter = async (searchNameTerm: string, sortBy: string = "createdAt", sortDirection: any = 'desc', pageNumber:number=1, pageSize:number=10) => {
-// формирование фильтра (может быть вынесено во вспомогательный метод)
-    const byName = searchNameTerm
-        ? {name: {$regex: searchNameTerm, $options: "i"}}
-        : {}
-
-    try {
-        // собственно запрос в бд (может быть вынесено во вспомогательный метод)
-        const items = await blogCollection
-            .find(byName)
-            .sort(sortBy, sortDirection)
-            .skip((pageNumber - 1) * pageSize)
-            .limit(Number(pageSize))
-            .toArray() as any[]
-        const totalCount = await blogCollection.countDocuments(byName)
-
-        // формирование ответа в нужном формате (может быть вынесено во вспомогательный метод)
-        return {
-            pagesCount: Math.ceil(totalCount / pageSize),
-            page: Number(pageNumber),
-            pageSize: Number(pageSize),
-            totalCount: totalCount,
-            items: items.map(value => blogMapper(value))
-        }
-    } catch (e) {
-        console.log(e)
-        return {error: 'some error'}
-    }
-}
 
 export const blogsRepository = {
-    async findBlogs(id: any, searchNameTerm:any, sortBy: any, sortDirection: any, pageNumber: number, pageSize: number){
-        return blogFilter(searchNameTerm, sortBy, sortDirection, pageNumber, pageSize)
-    },
-    async findBlogsById(id: string): Promise<BlogDbType | null>{
-        const newId = new ObjectId(id);
-        const blog = await blogCollection.findOne({_id: newId})
-        return blogMapper(blog)
-    },
     async createBlog(newBlog: any){
         const idOfNewBlog = await blogCollection.insertOne(newBlog)
-        const blog = await blogCollection.findOne({_id: idOfNewBlog.insertedId})
-        return blogMapper(blog);
+        return idOfNewBlog.insertedId; //нужно чтобы возвращался только id
     },
     async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean>{
         const objId = new ObjectId(id);
