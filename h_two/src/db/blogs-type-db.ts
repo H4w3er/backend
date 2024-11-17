f9bb7fc96c4c8efbda34e01255eaace634b7921f
H4w3er <mr.gavrik378@list.ru>
2024-11-17 19:10:58 +0300

    added more types of blogs for presentation layer

--- h_two/src/db/blogs-type-db.ts
+++ h_two/src/db/blogs-type-db.ts
@@ -1,8 +1,32 @@
 export type BlogDbType = {
-    //id: string
+    //_id: string
     name: string // max 15
     description: string // max 500
     websiteUrl: string // max 100 ^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$
     createdAt: string
     isMembership: boolean
+}
+export type BlogsUserViewType = {
+    "pagesCount": number,
+    "page": number,
+    "pageSize": number,
+    "totalCount": number,
+    "items": [
+    {
+        "id": "string",
+        "name": "string",
+        "description": "string",
+        "websiteUrl": "string",
+        "createdAt": "2024-11-17T15:47:16.266Z",
+        "isMembership": true
+    }
+]
+}
+export type BlogUserViewType = {
+    "id": "string",
+    "name": "string",
+    "description": "string",
+    "websiteUrl": "string",
+    "createdAt": "2024-11-17T15:47:16.266Z",
+    "isMembership": true
 }
