def8f65b8fe28168af1b1962dbcb4e76d4299bc1
H4w3er <mr.gavrik378@list.ru>
2024-11-21 17:13:38 +0300

    added logic for user route

--- h_two/src/settings.ts
+++ h_two/src/settings.ts
@@ -5,7 +5,8 @@ export const SETTINGS = {
     PORT: process.env.PORT || 3001,
     PATH: {
         BLOGS: '/blogs',
-        POSTS: '/posts'
+        POSTS: '/posts',
+        USERS: '/users'
     },
     MONGO_URI: process.env.MONGO_URL || 'mongodb://localhost:27017',
     DB_NAME: process.env.DB_NAME || ''
