bfd72dd1f81e6018c1e1aa50cd074f9a406fe4c4
H4w3er <mr.gavrik378@list.ru>
2024-11-24 14:48:27 +0300

    updated logic for user service

--- h_two/src/domain/users-service.ts
+++ h_two/src/domain/users-service.ts
@@ -1,20 +1,61 @@
 import {usersRepository} from "../repositories/users-db-repository";
+import {UserDbType} from "../db/user-type-db";
+import {ObjectId} from "mongodb";
 import bcrypt from "bcrypt";
+
 
 export const usersService = {
     async createUser(login: string, password: string, email: string) {
+        if (await usersRepository.findByLoginOrEmail(login)){
+                return 1
+            // return {
+            //     "errorsMessages":[
+            //         {
+            //             "message": "the login is not unique",
+            //             "field": "login"
+            //         }
+            //     ]
+            // }
+        }
+        if (await usersRepository.findByLoginOrEmail(email)){
+             return 2
+            //{
+            //     "errorsMessages":[
+            //         {
+            //             "message": "the email address is not unique",
+            //             "field": "email"
+            //         }
+            //     ]
+            // }
+        }
         const bcrypt = require('bcrypt')
         const passwordSalt = await bcrypt.genSalt(10)
-
-        const newUser = {
-            login: login,
-            password: password,
-            email: email,
+        const passwordHash = await this.createHash(password, passwordSalt)
+        const newUser:UserDbType = {
+            _id: new ObjectId(),
+            userName: login,
+            email,
+            passwordHash,
+            passwordSalt,
             createdAt: new Date().toISOString()
         }
         return usersRepository.createUser(newUser)
     },
-    async deletePost(id: string): Promise<boolean> {
+    async deleteUser(id: string): Promise<boolean> {
         return usersRepository.deleteUser(id)
+    },
+    async checkCredentials(loginOrEmail: string, password: string){
+         const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
+         if (!user) return false
+         const passwordHash = await this.createHash(password, user.passwordSalt)
+         if (user.passwordHash !== passwordHash){
+             return false
+         }
+         return true;
+    },
+    async createHash(password: string, salt: string){
+        const bcrypt = require('bcrypt')
+        const hash = await bcrypt.hash(password, salt)
+        return hash;
     }
 }
