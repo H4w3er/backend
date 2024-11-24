73ea0acd305dcadbd348d77c23f6cf42d5ebb3e9
H4w3er <mr.gavrik378@list.ru>
2024-11-24 14:47:30 +0300

    added logic for create new user

---
+++ h_two/src/routers/auth-router.ts
@@ -0,0 +1,13 @@
+import {Router} from "express";
+import {usersService} from "../domain/users-service";
+
+export const authRouter = Router({})
+
+authRouter.post('/login', async (req,res) =>{
+    const checkUser = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
+    if (checkUser){
+        res.status(204)
+    } else {
+        res.sendStatus(401)
+    }
+})
