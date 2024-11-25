303f543eeaba11025d0de70f136c5939b8233098
H4w3er <mr.gavrik378@list.ru>
2024-11-25 18:09:44 +0300

    added logic to auth users

--- h_two/src/routers/auth-router.ts
+++ h_two/src/routers/auth-router.ts
@@ -6,7 +6,7 @@ export const authRouter = Router({})
 authRouter.post('/login', async (req,res) =>{
     const checkUser = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
     if (checkUser){
-        res.status(204)
+        res.sendStatus(204)
     } else {
         res.sendStatus(401)
     }
