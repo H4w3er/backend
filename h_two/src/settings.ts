1b661dbcbb1c1b2fba7d9cee82d817fb19de27a8
H4w3er <mr.gavrik378@list.ru>
2024-11-27 17:01:36 +0300

    some update

--- h_two/src/settings.ts
+++ h_two/src/settings.ts
@@ -10,5 +10,6 @@ export const SETTINGS = {
         AUTH: '/auth'
     },
     MONGO_URI: process.env.MONGO_URL || 'mongodb://localhost:27017',
-    DB_NAME: process.env.DB_NAME || ''
+    DB_NAME: process.env.DB_NAME || '',
+    JWT_SECRET: process.env.JWT_SECRET || "123"
 }
