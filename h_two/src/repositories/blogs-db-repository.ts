4683128489f2cfef33a10ad6396c185ea4286687
Artem <mr.gavrik378@list.ru>
2024-12-01 17:00:28 +0300

    some update

--- h_two/src/repositories/blogs-db-repository.ts
+++ h_two/src/repositories/blogs-db-repository.ts
@@ -1,12 +1,10 @@
-import {blogCollection, postCollection} from "../db/mongo-db";
-import {ObjectId} from "mongodb";
-import {postsRepository} from "./posts-db-repository";
+import {blogCollection, postCollection, userCollection} from "../db/mongo-db";import {ObjectId} from "mongodb";
 
 
 export const blogsRepository = {
     async createBlog(newBlog: any){
         const idOfNewBlog = await blogCollection.insertOne(newBlog)
-        return idOfNewBlog.insertedId; //нужно чтобы возвращался только id
+        return idOfNewBlog.insertedId;
     },
     async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean>{
         const objId = new ObjectId(id);
@@ -21,21 +19,12 @@ export const blogsRepository = {
     async deleteAll(): Promise<boolean>{
         await blogCollection.deleteMany()
         await postCollection.deleteMany()
+        await userCollection.deleteMany()
         return true;
     },
     async isBlog(blogId: string): Promise<boolean>{
         const id = new ObjectId(blogId)
         const result = await blogCollection.countDocuments({_id: id})
         return result === 1;
-    },
-    async postsForBlog(blogId: string, sortBy: any, sortDirection: any, pageNumber: number, pageSize: number) {
-        if(await this.isBlog(blogId)) {
-            return postsRepository.findPostByBlogId(blogId, sortBy, sortDirection, pageNumber, pageSize)
-        } else return null
-    },
-    async createPostForBlog(id: string, title: string, shortDescription: string, content: string){
-        if(await this.isBlog(id)) {
-            return postsRepository.createPost(title, shortDescription, content, id)
-        } else return null
     }
 }
