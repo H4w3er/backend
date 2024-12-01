240e398cb67022e31c9bf9f7510e9af30c9ad769
Artem <mr.gavrik378@list.ru>
2024-12-01 17:04:56 +0300

    created type in db for comments

---
+++ h_two/src/db/comments-type-db.ts
@@ -0,0 +1,12 @@
+import {ObjectId} from "mongodb";
+
+export type CommentsDbType = {
+    "_id": ObjectId,
+    "content": string,
+    "commentatorInfo": {
+        "userId": string,
+        "userLogin": string
+    },
+    "createdAt": string,
+    "postId": ObjectId
+}
