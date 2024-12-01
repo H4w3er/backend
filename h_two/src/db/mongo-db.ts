2f8d455072e75ef131505755012a4e6be7685d15
Artem <mr.gavrik378@list.ru>
2024-12-01 17:01:26 +0300

    added collection for comments

--- h_two/src/db/mongo-db.ts
+++ h_two/src/db/mongo-db.ts
@@ -2,6 +2,8 @@ import {SETTINGS} from "../settings";
 import {Collection, Db, MongoClient} from "mongodb";
 import {BlogDbType} from "./blogs-type-db";
 import {PostDbType} from "./posts-type-db";
+import {UserDbType} from "./user-type-db";
+import {CommentsDbType} from "./comments-type-db";
 
 // получение доступа к бд
 const client: MongoClient = new MongoClient(SETTINGS.MONGO_URI)
@@ -10,6 +12,8 @@ export const db: Db = client.db(SETTINGS.DB_NAME);
 // получение доступа к коллекциям
 export const blogCollection: Collection<BlogDbType> = db.collection<BlogDbType>(SETTINGS.PATH.BLOGS)
 export const postCollection: Collection<PostDbType> = db.collection<PostDbType>(SETTINGS.PATH.POSTS)
+export const userCollection: Collection<UserDbType> = db.collection<UserDbType>(SETTINGS.PATH.USERS)
+export const commentsCollection: Collection<CommentsDbType> = db.collection<CommentsDbType>(SETTINGS.PATH.COMMENTS)
 
 export async function runDb () {
     try {
