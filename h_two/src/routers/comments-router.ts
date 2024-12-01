84bdda1eed455b508268dac1d35fce7e25db6f14
Artem <mr.gavrik378@list.ru>
2024-12-01 17:04:34 +0300

    created router for comments

---
+++ h_two/src/routers/comments-router.ts
@@ -0,0 +1,26 @@
+import {Router} from "express";
+import {authBearerMiddleware} from "../middlewares/authBearerMiddleware";
+import {commentsService} from "../domain/comments-service";
+import {contentValidation} from "../middlewares/posts-validation";
+import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
+
+export const commentsRouter = Router()
+
+commentsRouter.get('/:id', authBearerMiddleware,
+    async (req, res) => {
+    const comment = await commentsService.getCommentById(req.params.id)
+        if (comment) res.status(200).send(comment)
+        res.sendStatus(404)
+})
+
+commentsRouter.put('/:commentId',
+    authBearerMiddleware,
+    contentValidation,
+    inputValidationMiddleware,
+    async (req,res) => {
+        const newComment = await commentsService.updateCommentById(req.params.commentId, req.body.content, req.user!._id)
+        if (newComment===1) res.sendStatus(403)
+        if (!newComment) res.sendStatus(404)
+        res.sendStatus(204)
+})
+
