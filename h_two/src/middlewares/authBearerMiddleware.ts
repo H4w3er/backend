f3b6312afad92a35523c0383a49c00f4c97bf994
Artem <mr.gavrik378@list.ru>
2024-12-01 17:04:11 +0300

    some update

---
+++ h_two/src/middlewares/authBearerMiddleware.ts
@@ -0,0 +1,20 @@
+import {NextFunction, Request, Response} from "express";
+import {jwtService} from "../application/jwt-service";
+import {usersService} from "../domain/users-service";
+
+export const authBearerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const auth = req.headers.authorization as string
+    if (!auth) {
+        res.send(401)
+        return
+    }
+
+    const token = req.headers.authorization?.split(' ')[1]
+    const userId = await jwtService.getIdByToken(token)
+
+    if (userId){
+        req.user = await usersService.findUserById(userId)
+        next()
+    }
+    res.send(401)
+}
