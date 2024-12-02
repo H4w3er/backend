5f45ca0b45a45a4110ab110002d8d9602d952771
H4w3er <mr.gavrik378@list.ru>
2024-12-02 21:17:37 +0300

    fixed issue

--- h_two/src/middlewares/authBearerMiddleware.ts
+++ h_two/src/middlewares/authBearerMiddleware.ts
@@ -5,16 +5,16 @@ import {usersService} from "../domain/users-service";
 export const authBearerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const auth = req.headers.authorization as string
     if (!auth) {
-        res.send(401)
+        res.sendStatus(401)
         return
     }
 
     const token = req.headers.authorization?.split(' ')[1]
     const userId = await jwtService.getIdByToken(token)
-
     if (userId){
         req.user = await usersService.findUserById(userId)
         next()
+        return
     }
-    res.send(401)
+    res.sendStatus(401)
 }
