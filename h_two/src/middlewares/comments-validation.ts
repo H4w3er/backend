f6e3529961d26e0f905eeef466db05525f3414db
H4w3er <mr.gavrik378@list.ru>
2024-12-02 21:39:37 +0300

    some fix

--- h_two/src/middlewares/comments-validation.ts
+++ h_two/src/middlewares/comments-validation.ts
@@ -1,3 +1,3 @@
 import {body} from "express-validator";
 
-export const contentValidation = body('content').trim().isLength({min:20, max:300}).withMessage("Content should be more than 20 and less than 300");
+export const contentCommentValidation = body('content').trim().isLength({min:20, max:300}).withMessage("Content should be more than 20 and less than 300");
