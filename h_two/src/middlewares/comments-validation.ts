5761818da12eafc5e1774a2ac3ee15838d3bd314
H4w3er <mr.gavrik378@list.ru>
2024-12-07 14:23:13 +0300

    added id validator

--- h_two/src/middlewares/comments-validation.ts
+++ h_two/src/middlewares/comments-validation.ts
@@ -1,3 +1,4 @@
-import {body} from "express-validator";
+import {body, param} from "express-validator";
 
 export const contentCommentValidation = body('content').trim().isLength({min:20, max:300}).withMessage("Content should be more than 20 and less than 300");
+export const postIdCommentValidation = param('postId').trim().isLength({min:24, max:24}).withMessage("Post id should be 24 characters length");
