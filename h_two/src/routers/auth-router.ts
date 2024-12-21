2f721c79b5125f82c14bcc29b3edacd20f5c3e38
H4w3er <mr.gavrik378@list.ru>
2024-12-21 18:17:28 +0300

    added endpoints for registration, email confirm and resend verify code

--- h_two/src/routers/auth-router.ts
+++ h_two/src/routers/auth-router.ts
@@ -2,8 +2,10 @@ import {Router} from "express";
 import {usersService} from "../domain/users-service";
 import {jwtService} from "../application/jwt-service";
 import {authBearerMiddleware} from "../middlewares/authBearerMiddleware";
-import nodemailer from 'nodemailer'
 import {emailAdapter} from "../adapters/emailAdapter";
+import {emailValidation, loginValidation, passwordValidation} from "../middlewares/auth-validation";
+import {authService} from "../domain/auth-service";
+import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 
 export const authRouter = Router({})
 
@@ -24,7 +26,40 @@ authRouter.get('/me', authBearerMiddleware, async (req, res) =>{
     }
     res.status(200).send(thisUser)
 })
-authRouter.post('/registration', async (req, res) =>{
-    await emailAdapter.sendEmail(req.body.email)
-    res.sendStatus(200)
+authRouter.post('/registration-email-resending', emailValidation, inputValidationMiddleware, async (req, res) =>{
+    const result = await authService.sendConfirmationLetter(req.body.email)
+    if (result) res.sendStatus(204)
+    else res.status(400).send({"errorsMessages": [
+            {
+                "message": "something went wrong",
+                "field": "email"
+            }]})
+})
+authRouter.post('/registration', loginValidation, emailValidation, passwordValidation, inputValidationMiddleware, async (req, res) =>{
+    const newUser = await authService.createUser(req.body.login, req.body.password, req.body.email)
+    if(newUser === 1) {
+        res.status(400).send({"errorsMessages": [
+                {
+                    "message": "the login is not unique",
+                    "field": "login"
+                }]})
+    }
+    else if(newUser == 2){
+        res.status(400).send({"errorsMessages":[
+                {
+                    "message": "the email address is not unique",
+                    "field": "email"
+                }]})
+    } else{
+        res.sendStatus(204)
+    }
+})
+authRouter.post('/registration-confirmation', async (req, res) =>{
+    const verify = await emailAdapter.checkCode(req.body.code)
+    if (verify) res.sendStatus(204)
+    else res.status(400).send({"errorsMessages": [
+            {
+                "message": "wrong code",
+                "field": "code"
+            }]})
 })
