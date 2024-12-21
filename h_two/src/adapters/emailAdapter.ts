7f8ba8f88866231eddcb554be5ca6c8c0bf56c3c
H4w3er <mr.gavrik378@list.ru>
2024-12-21 18:18:29 +0300

    added logic for checking verifying code

--- h_two/src/adapters/emailAdapter.ts
+++ h_two/src/adapters/emailAdapter.ts
@@ -1,8 +1,9 @@
 import nodemailer from "nodemailer";
 import {SETTINGS} from "../settings";
+import {usersRepository} from "../repositories/users-db-repository";
 
 export const emailAdapter = {
-    async sendEmail(email: string) {
+    async sendEmail(email: string, code: string) {
         const transporter = nodemailer.createTransport({
             host: 'smtp.mail.ru',
             port: 465,
@@ -16,7 +17,14 @@ export const emailAdapter = {
             from: '"Artem"<mr.gavrik378@list.ru>',
             to: email,
             subject: 'account creation',
-            html: '<h1>Thanks for your registration</h1>\n <p>To finish registration please follow the link below:\n  <a href=\'https://somesite.com/confirm-email?code=your_confirmation_code\'>complete registration</a>\n </p>'
+            html: `https://somesite.com/confirm-email?code=${code}`
         });
+    },
+    async checkCode(code: string){
+        const conformation = await usersRepository.findUserByCode(code)
+        if (conformation) {
+            await usersRepository.updateUserByCode(code)
+            return true
+        } else return false
     }
 }
