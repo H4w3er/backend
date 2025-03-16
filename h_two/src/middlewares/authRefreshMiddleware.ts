a0aed183a732591ecd7c0cc5e4db1723b7745518
H4w3er <mr.gavrik378@list.ru>
2025-03-16 12:31:18 +0300

    changed logic, now auth with bearer token not required

--- h_two/src/middlewares/authRefreshMiddleware.ts
+++ h_two/src/middlewares/authRefreshMiddleware.ts
@@ -3,11 +3,11 @@ import {jwtService} from "../application/jwt-service";
 import {usersService} from "../domain/users-service";
 
 export const authRefreshMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const auth = req.headers.authorization as string;
+    /*const auth = req.headers.authorization as string;
     if (!auth) {
         res.sendStatus(401);
         return;
-    }
+    }*/
     const refreshToken = req.cookies.refreshToken;
     const userId = await jwtService.getIdByToken(refreshToken)
     const check = await usersService.isTokenAllowed(refreshToken, userId)
