99572f8527aab2cf5d04f9b7badfc514cba4a27b
H4w3er <mr.gavrik378@list.ru>
2025-03-18 18:55:40 +0300

    added more tests for refresh-token and login endpoints

--- h_two/__tests__/e2e/auth.api.test.ts
+++ h_two/__tests__/e2e/auth.api.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest'
 import {app} from "../../src";
+import {cookie} from "express-validator";
+import * as Cookies from "nodemailer/lib/fetch/cookies";
 
 describe('Token validation', () => {
     beforeAll(async ()=>{
@@ -8,6 +10,7 @@ describe('Token validation', () => {
     afterAll(async ()=>{
         await request(app).delete('/testing/all-data')
     })
+
     it('should create new user', async () => {
         const response = await request(app)
             .post('/auth/registration')
@@ -25,13 +28,36 @@ describe('Token validation', () => {
                 "loginOrEmail": "asdasd",
                 "password": "string"
             })
+        const refreshTokenCookie = response.headers['set-cookie'];
+        expect(refreshTokenCookie).toBeDefined()
+
         expect(response.status).toBe(200)
+        expect(response.body.accessToken).toBeDefined()
         expect(response.body.accessToken).toMatch(/^([A-Za-z0-9_-]+).([A-Za-z0-9_-]+).([A-Za-z0-9_-]+)$/)
     });
 
-    /*it('should return invalid message for incorrect token', async () => {
+    it('should return new \'refresh\' and \'access\' tokens; status 200; ' +
+        'content: new JWT \'access\' token, new JWT \'refresh\' token in cookie (http only, secure)', async () => {
+        const responseRefreshToken = await request(app)
+            .post('/auth/login')
+            .send({
+                "loginOrEmail": "asdasd",
+                "password": "string"
+            })
+        const refreshToken = responseRefreshToken.headers['set-cookie']
         const response = await request(app)
-            .get('/check-token')
+            .post('/auth/refresh-token')
+            .set('Cookie', refreshToken);
+
+        const refreshTokenCookie = response.headers['set-cookie'];
+        //console.log(response.status)
+        expect(refreshTokenCookie).toBeDefined()
+        expect(response.status).toBe(200);
+        expect(response.body.accessToken).toMatch(/^([A-Za-z0-9_-]+).([A-Za-z0-9_-]+).([A-Za-z0-9_-]+)$/);
+    });
+    /*it('should return an error if the "refresh" token has become invalid; status 401', async () => {
+        const response = await request(app)
+            .post('/refresh-token')
             .set('Cookie', 'token=invalid_token');
 
         expect(response.status).toBe(401);
