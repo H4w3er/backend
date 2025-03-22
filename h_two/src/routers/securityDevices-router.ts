7f39b0895e3597a4e0fab32f0dd6f967108b87e3
H4w3er <mr.gavrik378@list.ru>
2025-03-22 13:48:27 +0300

    added route for tracking devices with active sessions

---
+++ h_two/src/routers/securityDevices-router.ts
@@ -0,0 +1,15 @@
+import {Router} from "express";
+
+export const securityDevicesRouter = Router({});
+
+securityDevicesRouter.get('/devices', async (req, res) =>{
+
+})
+
+securityDevicesRouter.delete('/devices', async (req, res) =>{
+
+})
+
+securityDevicesRouter.delete('/devices/:id', async (req, res) =>{
+
+})
