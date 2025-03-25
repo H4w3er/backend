8a7fe73cc0fae56f9575efad00d4e93a274d1f8d
H4w3er <mr.gavrik378@list.ru>
2025-03-25 20:08:16 +0300

    created logic to get active sessions

--- h_two/src/routers/securityDevices-router.ts
+++ h_two/src/routers/securityDevices-router.ts
@@ -1,15 +1,18 @@
 import {Router} from "express";
+import {authRefreshMiddleware} from "../middlewares/authRefreshMiddleware";
+import {securityDevicesService} from "../domain/securityDevices-service";
 
 export const securityDevicesRouter = Router({});
 
-securityDevicesRouter.get('/devices', async (req, res) =>{
+securityDevicesRouter.get('/devices', authRefreshMiddleware, async (req, res) =>{
+    const sessions = await securityDevicesService.getActiveSessions()
+    res.send(sessions)
+})
+
+securityDevicesRouter.delete('/devices', authRefreshMiddleware, async (req, res) =>{
 
 })
 
-securityDevicesRouter.delete('/devices', async (req, res) =>{
-
-})
-
-securityDevicesRouter.delete('/devices/:id', async (req, res) =>{
+securityDevicesRouter.delete('/devices/:id', authRefreshMiddleware, async (req, res) =>{
 
 })
