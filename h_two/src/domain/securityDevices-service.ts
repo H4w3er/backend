f75edffe348bbd21cefb7d19564bc2995fffdab4
H4w3er <mr.gavrik378@list.ru>
2025-03-25 20:05:57 +0300

    added business layer for tracking sessions

---
+++ h_two/src/domain/securityDevices-service.ts
@@ -0,0 +1,11 @@
+import {securityDevicesDbRepository} from "../repositories/securityDevices-db-repository";
+
+export const securityDevicesService = {
+    async getActiveSessions(){
+        return await securityDevicesDbRepository.getActiveSessions()
+    },
+    async addNewSession(ip:string, title: string, lastActiveDate: string, deviceId: string, issuedAt: string, validUntil: string){
+        await securityDevicesDbRepository.addNewSession(ip, title, lastActiveDate, deviceId, issuedAt, validUntil)
+        return 0
+    }
+}
