d9c42f62a7ebb55e0f9d99e67ac350bc8803b799
H4w3er <mr.gavrik378@list.ru>
2025-03-26 22:24:05 +0300

    created working logic for get sessions

--- h_two/src/repositories/securityDevices-db-repository.ts
+++ h_two/src/repositories/securityDevices-db-repository.ts
@@ -1,9 +1,22 @@
 import {refreshTokenCollection} from "../db/mongo-db";
 
+const sessionsMapper = async (value: Array<any>) => {
+    const mappedSessions = value.map(session => {
+        session = {
+            ip: session.ip,
+            title: session.title,
+            lastActiveDate: session.lastActiveDate,
+            deviceId: session.deviceId
+        }
+        return session
+    })
+    return mappedSessions
+}
+
 export const securityDevicesDbRepository={
     async getActiveSessions() {
-        const sessions = await refreshTokenCollection.find();
-        return sessions
+        const sessions = await refreshTokenCollection.find({}).toArray() as any[];
+        return sessionsMapper(sessions)
     },
     async addNewSession(ip:string, title: string, lastActiveDate: string, deviceId: string, issuedAt: string, validUntil: string){
         await refreshTokenCollection.insertOne({ip, title, lastActiveDate, deviceId, issuedAt, validUntil})
