4eb24331eeabcde43a5ef325a4d09282344e7474
H4w3er <mr.gavrik378@list.ru>
2025-03-28 19:06:17 +0300

    added logic for adding device id in session db

--- h_two/src/application/jwt-service.ts
+++ h_two/src/application/jwt-service.ts
@@ -4,24 +4,41 @@ import {ObjectId} from "mongodb";
 import {securityDevicesDbRepository} from "../repositories/securityDevices-db-repository";
 
 export const jwtService = {
-    async createJWT(id: ObjectId){
+    async createJWT(id: ObjectId) {
         const jwt = require('jsonwebtoken')
-        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET,{expiresIn: '10 seconds'})
+        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET, {expiresIn: '10 seconds'})
         return token
     },
-    async getIdByToken(token: string | undefined){
-        try{
+    async getIdByToken(token: string | undefined) {
+        try {
             const jwt = require('jsonwebtoken')
             const result = jwt.verify(token, SETTINGS.JWT_SECRET)
             return result.userId
-        } catch (error){
+        } catch (error) {
             return null
         }
     },
-    async createRefreshToken(id: ObjectId, deviceName: string| undefined){
+    async createRefreshToken(id: ObjectId, deviceName: string | undefined) {
         const activeSession = await securityDevicesDbRepository.getActiveSessionsByDeviceName(deviceName)
+        const activeSessionCount = activeSession.length
         const jwt = require('jsonwebtoken')
-        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET,{expiresIn: '20 seconds'}, {deviceId: '1'})
-        return token
+        /*if (activeSessionCount > 0) {
+        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET, {expiresIn: '20 seconds'}, {deviceId: activeSession[activeSessionCount - 1].deviceId + 1})
+        return {
+            token: token,
+            deviceId: activeSession[activeSessionCount - 1].deviceId + 1
+        }
+    } else {
+        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET, {expiresIn: '20 seconds'}, {deviceId: 1})
+        return {
+            token: token,
+            deviceId: 1
+        }
+    }*/
+        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET, {expiresIn: '20 seconds'}, {deviceId: 1})
+        return {
+            token: token,
+            deviceId: '1'
+        }
     }
 }
