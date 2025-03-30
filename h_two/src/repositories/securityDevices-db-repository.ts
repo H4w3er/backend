1024bb7f0333cdbdb67457f8199820006e236cd3
H4w3er <mr.gavrik378@list.ru>
2025-03-30 12:24:32 +0300

    now we track userid

--- h_two/src/repositories/securityDevices-db-repository.ts
+++ h_two/src/repositories/securityDevices-db-repository.ts
@@ -1,5 +1,6 @@
 import {refreshTokenCollection} from "../db/mongo-db";
 import {refreshTokenDb} from "../db/refresh-Token-db";
+import {ObjectId} from "mongodb";
 
 const sessionsMapper = async (value: Array<any>) => {
     const mappedSessions = value.map(session => {
@@ -19,14 +20,14 @@ export const securityDevicesDbRepository={
         const sessions = await refreshTokenCollection.find({}).toArray() as any[];
         return sessionsMapper(sessions)
     },
-    async addNewSession(ip:string, title: string|undefined, lastActiveDate: string, deviceId: string, issuedAt: Date, validUntil: string){
-        await refreshTokenCollection.insertOne({ip, title, lastActiveDate, deviceId, issuedAt, validUntil})
-        console.log(await refreshTokenCollection.find({}).toArray() as any[])
+    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: string, issuedAt: Date, validUntil: string, userId:ObjectId){
+        await refreshTokenCollection.insertOne({ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId})
+        //console.log(await refreshTokenCollection.find({}).toArray() as any[])
         return 0
     },
-    async getActiveSessionsByDeviceName(deviceName : string|undefined){
-        const activeSession = await refreshTokenCollection.find({title: deviceName}).toArray() as any//Array<refreshTokenDb>
-        const validActiveSession = activeSession.filter(session => (session.issuedAt.getSeconds()) < (new Date().getSeconds()))
-        return validActiveSession;
+    async getActiveSessionsByUserId(userId : ObjectId){
+        const activeSession = await refreshTokenCollection.find({userId: userId}).toArray() as any//Array<refreshTokenDb>
+        //const validActiveSession = activeSession.filter((session: { issuedAt: { getSeconds: () => any; }; }) => (session.issuedAt.getSeconds()) < (new Date().getSeconds()))
+        return activeSession;
     }
 }
