b5288e43174bb958cc0d3b6d11437f525ef4de21
H4w3er <mr.gavrik378@list.ru>
2025-04-03 16:23:42 +0300

    created logic for deleting other active sessions

--- h_two/src/routers/securityDevices-router.ts
+++ h_two/src/routers/securityDevices-router.ts
@@ -1,18 +1,25 @@
 import {Router} from "express";
 import {authRefreshMiddleware} from "../middlewares/authRefreshMiddleware";
 import {securityDevicesService} from "../domain/securityDevices-service";
+import {jwtService} from "../application/jwt-service";
 
 export const securityDevicesRouter = Router({});
 
 securityDevicesRouter.get('/devices', authRefreshMiddleware, async (req, res) =>{
-    const sessions = await securityDevicesService.getActiveSessions()
-    res.send(sessions)
+    const refreshToken = req.cookies.refreshToken;
+    const userId = await jwtService.getIdByToken(refreshToken)
+    const sessions = await securityDevicesService.getActiveSessions(userId)
+    res.status(200).send(sessions)
 })
 
 securityDevicesRouter.delete('/devices', authRefreshMiddleware, async (req, res) =>{
-
+    const refreshToken = req.cookies.refreshToken;
+    const userId = await jwtService.getIdByToken(refreshToken)
+    const deviceId = await jwtService.getDeviceIdByToken(refreshToken)
+    await securityDevicesService.deleteAllOther(userId, deviceId)
+    res.sendStatus(204)
 })
 
 securityDevicesRouter.delete('/devices/:id', authRefreshMiddleware, async (req, res) =>{
-
+    await securityDevicesService.deleteSessionByDeviceId()
 })
