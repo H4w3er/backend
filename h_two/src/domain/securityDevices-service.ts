2cf56f050c72fcf61fc2d8a2690a27e35e34df46
H4w3er <mr.gavrik378@list.ru>
2025-04-03 16:26:44 +0300

    added endpoint for delete all other sessions

--- h_two/src/domain/securityDevices-service.ts
+++ h_two/src/domain/securityDevices-service.ts
@@ -3,16 +3,20 @@ import {ObjectId} from "mongodb";
 import {securityDevicesRouter} from "../routers/securityDevices-router";
 
 export const securityDevicesService = {
-    async getActiveSessions(){
-        return await securityDevicesDbRepository.getActiveSessions()
+    async getActiveSessions(userId: ObjectId){
+        return await securityDevicesDbRepository.getActiveSessions(userId)
     },
-    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: string, userId: ObjectId){
+    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: Date, userId: ObjectId){
         const activeSessions = await securityDevicesDbRepository.getActiveSessionsByUserId(userId)
         await securityDevicesDbRepository.addNewSession(ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId)
         return 0
     },
     async getActiveSessionsByUserId(userId: ObjectId){
         return await securityDevicesDbRepository.getActiveSessionsByUserId(userId)
+    },
+    async deleteAllOther(userId: ObjectId, deviceId: ObjectId){
+        await securityDevicesDbRepository.deleteAllOther(userId, deviceId)
+        return 0
     }
 
 }
