1a757f7683236db5bd9bf0116c5f673414099631
H4w3er <mr.gavrik378@list.ru>
2025-04-03 16:59:14 +0300

    added logic for deleting sessions by device id

--- h_two/src/domain/securityDevices-service.ts
+++ h_two/src/domain/securityDevices-service.ts
@@ -7,16 +7,25 @@ export const securityDevicesService = {
         return await securityDevicesDbRepository.getActiveSessions(userId)
     },
     async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: Date, userId: ObjectId){
-        const activeSessions = await securityDevicesDbRepository.getActiveSessionsByUserId(userId)
+        const activeSessions = await securityDevicesDbRepository.getSessionsByUserId(userId)
         await securityDevicesDbRepository.addNewSession(ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId)
         return 0
     },
     async getActiveSessionsByUserId(userId: ObjectId){
-        return await securityDevicesDbRepository.getActiveSessionsByUserId(userId)
+        return await securityDevicesDbRepository.getSessionsByUserId(userId)
     },
     async deleteAllOther(userId: ObjectId, deviceId: ObjectId){
         await securityDevicesDbRepository.deleteAllOther(userId, deviceId)
         return 0
+    },
+    async deleteSessionByDeviceId(deviceId: ObjectId, userId: ObjectId){
+        const session = await securityDevicesDbRepository.getSessionsByDeviceId(deviceId)
+        if (!session) return 1
+        else if (session?.userId!=userId) return 0
+        else {
+            const deleted = await securityDevicesDbRepository.deleteSessionByDeviceId(deviceId)
+            if (deleted.deletedCount === 0) return 1
+            else return 2
+        }
     }
-
 }
