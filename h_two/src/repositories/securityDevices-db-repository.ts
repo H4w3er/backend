807ef0743da47e4a0c20c0796551efbf112eb437
H4w3er <mr.gavrik378@list.ru>
2025-04-03 16:27:06 +0300

    added logic for deleting all other sessions

--- h_two/src/repositories/securityDevices-db-repository.ts
+++ h_two/src/repositories/securityDevices-db-repository.ts
@@ -16,11 +16,11 @@ const sessionsMapper = async (value: Array<any>) => {
 }
 
 export const securityDevicesDbRepository={
-    async getActiveSessions() {
-        const sessions = await refreshTokenCollection.find({}).toArray() as any[];
+    async getActiveSessions(userId: ObjectId) {
+        const sessions = await refreshTokenCollection.find({userId: new ObjectId(userId)}).toArray() as any[];
         return sessionsMapper(sessions)
     },
-    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: string, userId:ObjectId){
+    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: Date, userId:ObjectId){
         await refreshTokenCollection.insertOne({ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId})
         //console.log(await refreshTokenCollection.find({}).toArray() as any[])
         return 0
@@ -29,5 +29,9 @@ export const securityDevicesDbRepository={
         const activeSession = await refreshTokenCollection.find({userId: userId}).toArray() as any//Array<refreshTokenDb>
         //const validActiveSession = activeSession.filter((session: { issuedAt: { getSeconds: () => any; }; }) => (session.issuedAt.getSeconds()) < (new Date().getSeconds()))
         return activeSession;
+    },
+    async deleteAllOther(userId: ObjectId, deviceId: ObjectId){
+        await refreshTokenCollection.deleteMany({validUntil:{$lt: new Date()}, userId: new ObjectId(userId), deviceId:{$ne: deviceId}})
+        return 0
     }
 }
