95428f520dda673fd08f59113323db0cfe2cf167
H4w3er <mr.gavrik378@list.ru>
2025-04-03 17:00:12 +0300

    added filter for getActiveSessions and added logic for delete session by device id

--- h_two/src/repositories/securityDevices-db-repository.ts
+++ h_two/src/repositories/securityDevices-db-repository.ts
@@ -18,20 +18,31 @@ const sessionsMapper = async (value: Array<any>) => {
 export const securityDevicesDbRepository={
     async getActiveSessions(userId: ObjectId) {
         const sessions = await refreshTokenCollection.find({userId: new ObjectId(userId)}).toArray() as any[];
-        return sessionsMapper(sessions)
+        const activeSessions = sessions.filter(session => session.validUntil >  new Date()
+        )
+        console.log(activeSessions)
+        return sessionsMapper(activeSessions)
     },
     async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: Date, userId:ObjectId){
         await refreshTokenCollection.insertOne({ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId})
         //console.log(await refreshTokenCollection.find({}).toArray() as any[])
         return 0
     },
-    async getActiveSessionsByUserId(userId : ObjectId){
+    async getSessionsByUserId(userId : ObjectId){
         const activeSession = await refreshTokenCollection.find({userId: userId}).toArray() as any//Array<refreshTokenDb>
         //const validActiveSession = activeSession.filter((session: { issuedAt: { getSeconds: () => any; }; }) => (session.issuedAt.getSeconds()) < (new Date().getSeconds()))
         return activeSession;
     },
+    async getSessionsByDeviceId(deviceId: ObjectId){
+        const session = await refreshTokenCollection.findOne({deviceId: deviceId})
+        return session
+    },
     async deleteAllOther(userId: ObjectId, deviceId: ObjectId){
         await refreshTokenCollection.deleteMany({validUntil:{$lt: new Date()}, userId: new ObjectId(userId), deviceId:{$ne: deviceId}})
         return 0
+    },
+    async deleteSessionByDeviceId(deviceId: ObjectId){
+        const deleted = await refreshTokenCollection.deleteOne({deviceId: deviceId})
+        return deleted
     }
 }
