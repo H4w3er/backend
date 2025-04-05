22f049cece08e89511be9af61895ebffb8196352
H4w3er <mr.gavrik378@list.ru>
2025-04-05 21:54:59 +0300

    fixed issue, now work correct

--- h_two/src/middlewares/CRL-cheking.ts
+++ h_two/src/middlewares/CRL-cheking.ts
@@ -2,12 +2,12 @@ import {Request, Response, NextFunction} from "express";
 import {requestsToApiCollection} from "../db/mongo-db";
 
 export const CRLChecking = async (req: Request, res: Response, next: NextFunction)=> {
-    const ip = req.headers['x-forwarded-for']
-    const url = req.baseUrl
+    const ip = req.ip
+    const url = req.originalUrl
     const date = new Date()
-    date.setSeconds(date.getSeconds() - 10)
     if (ip!=undefined) {
         await requestsToApiCollection.insertOne({IP: ip, URL: url, date: date});
+        date.setSeconds(date.getSeconds() - 10)
         const requestsCount = await requestsToApiCollection.find({IP: ip, URL: url, date: {$gte: date}}).toArray()
         if (requestsCount.length > 5) res.sendStatus(429)
         else next()
