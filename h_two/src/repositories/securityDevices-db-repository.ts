c7aecb1db5f448ac8197cd454d5be4438acfeb57
H4w3er <mr.gavrik378@list.ru>
2025-04-10 19:37:39 +0300

    rewrited logic in deleteAllOther

--- h_two/src/repositories/securityDevices-db-repository.ts
+++ h_two/src/repositories/securityDevices-db-repository.ts
@@ -1,5 +1,7 @@
 import {refreshTokenCollection} from "../db/mongo-db";
-import {ObjectId} from "mongodb";
+import {ObjectId, WithId} from "mongodb";
+import {refreshTokenDb} from "../db/refresh-Token-db";
+import {log} from "node:util";
 
 const sessionsMapper = async (value: Array<any>) => {
     //const activeSessions = value.filter(session => session.validUntil >=  new Date())
@@ -10,6 +12,7 @@ const sessionsMapper = async (value: Array<any>) => {
             lastActiveDate: session.lastActiveDate,
             deviceId: session.deviceId
     })
+    //console.log(mappedSessions)
     return mappedSessions
 }
 
@@ -18,7 +21,7 @@ export const securityDevicesDbRepository={
         const sessions = await refreshTokenCollection.find({userId: new ObjectId(userId)}).toArray() as any[];
         return sessionsMapper(sessions)
     },
-    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: Date, userId:ObjectId, refreshToken: string){
+    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: string, issuedAt: Date, validUntil: Date, userId:ObjectId, refreshToken: string){
         await refreshTokenCollection.insertOne({ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId, refreshToken})
         return 0
     },
@@ -26,21 +29,26 @@ export const securityDevicesDbRepository={
         const activeSession = await refreshTokenCollection.find({userId: userId}).toArray() as any//Array<refreshTokenDb>
         return activeSession;
     },
-    async getSessionsByDeviceId(deviceId: ObjectId){
+    async getSessionsByDeviceId(deviceId: string){
         const session = await refreshTokenCollection.findOne({deviceId: deviceId})
         return session
     },
-    async deleteAllOther(userId: ObjectId, deviceId: ObjectId){
-        await refreshTokenCollection.deleteMany({userId: new ObjectId(userId), deviceId:{$ne: new ObjectId(deviceId)}})
-        return 0
+    async deleteAllOther(userId: ObjectId, deviceId: string){
+        console.log(`deleteAllOther userId:`, userId)
+        console.log(`deleteAllOther deviceId:`, deviceId)
+        const thisSession = await this.getSessionsByDeviceId(deviceId)
+        console.log('this session', thisSession)
+        await refreshTokenCollection.deleteMany({userId: new ObjectId(userId)})
+        await refreshTokenCollection.insertOne(thisSession!)
+
     },
-    async deleteSessionByDeviceId(deviceId: ObjectId){
+    async deleteSessionByDeviceId(deviceId: string){
         const deleted = await refreshTokenCollection.deleteOne({deviceId: deviceId})
         return deleted
     },
-    async sessionUpdate(userId: ObjectId, deviceId: ObjectId, refreshToken: string){
+    async sessionUpdate(userId: ObjectId, deviceId: string, refreshToken: string){
         const date = new Date().toISOString()
-        await refreshTokenCollection.updateOne({userId: new ObjectId(userId), deviceId: new ObjectId(deviceId)}, {$set: {lastActiveDate: date, refreshToken: refreshToken}})
+        await refreshTokenCollection.updateOne({userId: new ObjectId(userId), deviceId: deviceId}, {$set: {lastActiveDate: date, refreshToken: refreshToken}})
         return 0
     }
 }
