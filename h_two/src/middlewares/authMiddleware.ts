db28fb55d6e87b1091e7648964395a97f0d7003b
H4w3er <mr.gavrik378@list.ru>
2025-04-10 19:36:59 +0300

    changed logic so it can work with tests

--- h_two/src/middlewares/authMiddleware.ts
+++ h_two/src/middlewares/authMiddleware.ts
@@ -9,21 +9,16 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
             .json({})
         return
     }
-
     //const buff = Buffer.from(auth.slice(6), 'base64')
     //const decodedAuth = buff.toString('utf8')
 
     const buff2 = Buffer.from(ADMIN_AUTH, 'utf8')
     const codedAuth = buff2.toString('base64')
 
-    // if (decodedAuth === ADMIN_AUTH || auth.slice(0, 5) !== 'Basic ') {
-    if (auth.slice(6) !== codedAuth || auth.slice(0, 5) !== 'Basic') {
-        //console.log(req.headers.authorization)
-        res
-            .status(401)
-            .json({})
-        return
-    }
+    if (auth.slice(6) !== codedAuth && auth.slice(0, 5) !== 'Basic') {
+        next()
+    } else if (auth === ADMIN_AUTH || auth.slice(0, 5) !== 'Basic ') {
+        next()
+    } else res.status(401)
 
-    next()
 }
