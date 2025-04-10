9e818ed9801cd3a0b35f5ebdc891c16f34a17ddd
H4w3er <mr.gavrik378@list.ru>
2025-04-10 19:36:28 +0300

    changed types in addNewSession

--- h_two/src/domain/securityDevices-service.ts
+++ h_two/src/domain/securityDevices-service.ts
@@ -6,7 +6,7 @@ export const securityDevicesService = {
     async getActiveSessions(userId: ObjectId){
         return await securityDevicesDbRepository.getActiveSessions(userId)
     },
-    async addNewSession(ip:string|undefined|string[], title: string|undefined, lastActiveDate: string, deviceId: ObjectId, issuedAt: Date, validUntil: Date, userId: ObjectId, refreshToken: string){
+    async addNewSession(ip:string, title: string | undefined, lastActiveDate: string, deviceId: string, issuedAt: Date, validUntil: Date, userId: ObjectId, refreshToken: string){
         const activeSessions = await securityDevicesDbRepository.getSessionsByUserId(userId)
         await securityDevicesDbRepository.addNewSession(ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId, refreshToken)
         return 0
@@ -14,21 +14,16 @@ export const securityDevicesService = {
     async getActiveSessionsByUserId(userId: ObjectId){
         return await securityDevicesDbRepository.getSessionsByUserId(userId)
     },
-    async deleteAllOther(userId: ObjectId, deviceId: ObjectId){
+    async deleteAllOther(userId: ObjectId, deviceId: string){
         await securityDevicesDbRepository.deleteAllOther(userId, deviceId)
         return 0
     },
     async deleteSessionByDeviceId(deviceId: string, userId: ObjectId){
-        try {
-            const session = await securityDevicesDbRepository.getSessionsByDeviceId(new ObjectId(deviceId))
-        } catch (er){
-            return 1
-        }
-        const session = await securityDevicesDbRepository.getSessionsByDeviceId(new ObjectId(deviceId))
+        const session = await securityDevicesDbRepository.getSessionsByDeviceId(deviceId)
         if (!session) return 1
         else if (session?.userId!=userId) return 0
         else {
-            const deleted = await securityDevicesDbRepository.deleteSessionByDeviceId(new ObjectId(deviceId))
+            const deleted = await securityDevicesDbRepository.deleteSessionByDeviceId(deviceId)
             if (deleted.deletedCount === 0) return 1
             else {
                 await usersService.addToBlackListAnother(session.refreshToken, session.userId)
@@ -36,7 +31,7 @@ export const securityDevicesService = {
             }
         }
     },
-    async sessionUpdate(userId: ObjectId, deviceId: ObjectId, refreshToken: string){
+    async sessionUpdate(userId: ObjectId, deviceId: string, refreshToken: string){
         await securityDevicesDbRepository.sessionUpdate(userId, deviceId, refreshToken)
         return 0
     }
