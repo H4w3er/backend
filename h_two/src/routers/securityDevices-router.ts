c668f7b3658eb9c20a8dc1c46620df96d1869bd1
H4w3er <mr.gavrik378@list.ru>
2025-04-11 22:21:00 +0300

    updated logic, half of it moved to service

--- h_two/src/routers/securityDevices-router.ts
+++ h_two/src/routers/securityDevices-router.ts
@@ -2,37 +2,35 @@ import {Router} from "express";
 import {authRefreshMiddleware} from "../middlewares/authRefreshMiddleware";
 import {securityDevicesService} from "../domain/securityDevices-service";
 import {jwtService} from "../application/jwt-service";
-import {usersService} from "../domain/users-service";
 
 export const securityDevicesRouter = Router({});
 
+const answersMap = new Map();
+answersMap.set("accessRejected", 403)
+answersMap.set("notFound", 404)
+answersMap.set("successDelete", 204)
+
 securityDevicesRouter.get('/devices', authRefreshMiddleware, async (req, res) =>{
     const refreshToken = req.cookies.refreshToken;
-    const userId = await jwtService.getIdByToken(refreshToken)
-    const deviceId = await jwtService.getDeviceIdByToken(refreshToken)
-    await securityDevicesService.sessionUpdate(userId, deviceId, refreshToken)
+    const verifiedToken = await jwtService.getIdFromToken(refreshToken)
+    await securityDevicesService.sessionUpdate(verifiedToken.userId, verifiedToken.deviceId, refreshToken)
 
-    const sessions = await securityDevicesService.getActiveSessions(userId)
-
+    const sessions = await securityDevicesService.getActiveSessions(verifiedToken.userId)
     res.status(200).send(sessions)
 })
 
 securityDevicesRouter.delete('/devices', authRefreshMiddleware, async (req, res) =>{
     const refreshToken = req.cookies.refreshToken;
-    console.log(`refToken`, refreshToken)
-    const deviceId = await jwtService.getDeviceIdByToken(refreshToken);
-    const userId = await jwtService.getIdByToken(refreshToken)
-    await securityDevicesService.deleteAllOther(userId, deviceId)
+    const verifiedToken = await jwtService.getIdFromToken(refreshToken)
+    await securityDevicesService.deleteAllOther(verifiedToken.userId, verifiedToken.deviceId)
     res.sendStatus(204)
 })
 
 securityDevicesRouter.delete('/devices/:id', authRefreshMiddleware, async (req, res) =>{
     const refreshToken = req.cookies.refreshToken;
-    const userId = await jwtService.getIdByToken(refreshToken)
     const deviceId = req.params.id
-    //await usersService.addToBlackListAnother(refreshToken, userId)
-    const deleted = await securityDevicesService.deleteSessionByDeviceId(deviceId, userId)
-    if (deleted === 0) res.sendStatus(403)
-    else if (deleted===1) res.sendStatus(404)
-    else res.sendStatus(204)
+    const userIdFromToken = await jwtService.getIdFromToken(refreshToken)
+    const deleted = await securityDevicesService.deleteSessionByDeviceId(deviceId, userIdFromToken.userId)
+
+    res.sendStatus(answersMap.get(deleted))
 })
