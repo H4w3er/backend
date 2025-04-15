5452ddaca2c0d2e699b76b83f19901de782e4566
H4w3er <mr.gavrik378@list.ru>
2025-04-15 17:31:08 +0300

    recreated logic using classes

--- h_two/src/routers/users-router.ts
+++ h_two/src/routers/users-router.ts
@@ -1,4 +1,5 @@
 import {Router} from "express";
+import {Request, Response} from 'express'
 import {usersQueryRepository} from "../repositories/users-db-query-repository";
 import {usersService} from "../domain/users-service";
 import {authMiddleware} from "../middlewares/authMiddleware";
@@ -7,45 +8,51 @@ import {inputValidationMiddleware} from "../middlewares/input-validation-middlew
 
 export const usersRouter = Router({})
 
-
-usersRouter.get('/',
-    authMiddleware,
-    async (req, res) =>{
-    // @ts-ignore
-    const users = await usersQueryRepository.getUsers(req.query.sortBy, req.query.sortDirection, req.query.pageNumber, req.query.pageSize, req.query.searchLoginTerm, req.query.searchEmailTerm)
-    res.status(200).send(users)
-})
-usersRouter.post('/',
-    authMiddleware,
-    loginValidation,
-    passwordValidation,
-    emailValidation,
-    inputValidationMiddleware,
-    async (req, res) => {
+class UsersController {
+    async getUsers(req: Request, res: Response) {
+        const sortBy = req.query.sortBy as string
+        const sortDirection = req.query.sortDirection as string
+        const pageNumber = req.query.pageNumber as string
+        const pageSize = req.query.pageSize as string
+        const searchLoginTerm = req.query.searchLoginTerm as string
+        const searchEmailTerm = req.query.searchEmailTerm as string
+        const users = await usersQueryRepository.getUsers(sortBy, sortDirection, pageNumber, pageSize, searchLoginTerm, searchEmailTerm)
+        res.status(200).send(users)
+    }
+    async createUser(req: Request, res: Response) {
         const newUser = await usersService.createUser(req.body.login, req.body.password, req.body.email)
-        if(newUser == 1) {
-            res.status(400).send({"errorsMessages": [
+        if (newUser == 1) {
+            res.status(400).send({
+                "errorsMessages": [
                     {
                         "message": "the login is not unique",
                         "field": "login"
-                    }]})
+                    }]
+            })
         }
-        if(newUser == 2){
-            res.status(400).send({"errorsMessages":[
+        if (newUser == 2) {
+            res.status(400).send({
+                "errorsMessages": [
                     {
                         "message": "the email address is not unique",
                         "field": "email"
-                    }]})
-        } else{
+                    }]
+            })
+        } else {
             res.status(201).send(newUser)
         }
-    })
-usersRouter.delete('/:id',
-    authMiddleware,
-    async (req, res) =>{
-    if (await usersService.deleteUser(req.params.id)){
-        res.sendStatus(204)
-    } else {
-        res.sendStatus(404)
     }
-})
+    async deleteUser(req: Request, res: Response) {
+        if (await usersService.deleteUser(req.params.id)) {
+            res.sendStatus(204)
+        } else {
+            res.sendStatus(404)
+        }
+    }
+}
+
+const usersController = new UsersController()
+
+usersRouter.get('/', authMiddleware, usersController.getUsers )
+usersRouter.post('/', authMiddleware, loginValidation, passwordValidation, emailValidation, inputValidationMiddleware, usersController.createUser)
+usersRouter.delete('/:id', authMiddleware, usersController.deleteUser)
