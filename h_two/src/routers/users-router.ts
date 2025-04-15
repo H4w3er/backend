d28fc6398f6787f058e7d78da0cc131ee46078e0
H4w3er <mr.gavrik378@list.ru>
2025-04-15 18:18:15 +0300

    updated logic

--- h_two/src/routers/users-router.ts
+++ h_two/src/routers/users-router.ts
@@ -20,30 +20,20 @@ class UsersController {
         res.status(200).send(users)
     }
     async createUser(req: Request, res: Response) {
-        const newUser = await usersService.createUser(req.body.login, req.body.password, req.body.email)
-        if (newUser == 1) {
-            res.status(400).send({
-                "errorsMessages": [
-                    {
-                        "message": "the login is not unique",
-                        "field": "login"
-                    }]
-            })
-        }
-        if (newUser == 2) {
-            res.status(400).send({
-                "errorsMessages": [
-                    {
-                        "message": "the email address is not unique",
-                        "field": "email"
-                    }]
-            })
-        } else {
+        const login = req.body.login as string
+        const password = req.body.password as string
+        const email = req.body.email as string
+        const newUser = await usersService.createUser(login, password, email)
+        if('id' in newUser) {
             res.status(201).send(newUser)
+        }
+        else{
+            res.status(400).send(newUser)
         }
     }
     async deleteUser(req: Request, res: Response) {
-        if (await usersService.deleteUser(req.params.id)) {
+        const id = req.params.id as string
+        if (await usersService.deleteUser(id)) {
             res.sendStatus(204)
         } else {
             res.sendStatus(404)
