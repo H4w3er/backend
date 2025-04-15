0abf0578883ebb8aee4894e331ad55ec1ea1ec83
H4w3er <mr.gavrik378@list.ru>
2025-04-15 18:18:46 +0300

    removed duplicate type of user

--- h_two/src/db/user-type-db.ts
+++ h_two/src/db/user-type-db.ts
@@ -1,19 +1,6 @@
 import {ObjectId} from "mongodb";
 
 
-export type UserDbType = {
-    "_id": ObjectId
-    "userName": string,
-    "email": string,
-    "passwordHash": string,
-    "passwordSalt": string,
-    "createdAt": string,
-    "emailConfirm":{
-        "confCode": string,
-        "isConfirmed": boolean
-    },
-    "refreshTokenBlackList": Array<string>
-}
 export type UserDbTypeCommon = {
     "_id": ObjectId,
     "userName": string,
