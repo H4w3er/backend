bb7b079362e4f48763414cebb7eac6b1a689a4eb
H4w3er <mr.gavrik378@list.ru>
2025-04-15 18:19:56 +0300

    recreated logic using classes

--- h_two/src/repositories/users-db-repository.ts
+++ h_two/src/repositories/users-db-repository.ts
@@ -3,48 +3,50 @@ import {ObjectId} from "mongodb";
 import {v4 as uuidv4} from "uuid";
 import {UserViewType} from "../db/user-type-db";
 
-export const usersRepository = {
+class UsersDbRepository{
     async createUser(newUser: any) {
         await userCollection.insertOne(newUser)
-        return userMapper(newUser);
-    },
+        return await this.userMapper(newUser);
+    }
     async deleteUser(id: string){
         const objId = new ObjectId(id);
         const result = await userCollection.deleteOne({_id: objId})
         return result.deletedCount === 1;
-    },
+    }
     async findByLoginOrEmail(loginOrEmail: string){
         const user = await userCollection.findOne({$or: [{email: loginOrEmail}, {userName: loginOrEmail}]})
         return user
-    },
+    }
     async findUserById(id: string){
         const newId = new ObjectId(id)
         const user = await userCollection.findOne({_id: newId})
         return user
-    },
+    }
     async findUserByCode(code: string){
         const user = await userCollection.findOne({'emailConfirm.confCode': code})
         return user
-    },
+    }
     async updateUserByCode(code: string){
         await userCollection.updateOne({'emailConfirm.confCode': code}, {$set:{'emailConfirm.isConfirmed': true}})
-    },
+    }
     async updateUserCodeByCode(code: string){
         const newCode = uuidv4()
         await userCollection.updateOne({'emailConfirm.confCode': code}, {$set:{'emailConfirm.confCode': newCode}})
         return newCode
-    },
+    }
     async addToBlackList(refreshToken: string, userId: string | ObjectId){
         await userCollection.updateOne({_id: new ObjectId(userId)}, {$push:{refreshTokenBlackList: refreshToken}})
         return true
     }
-}
-
-const userMapper = (value: any): UserViewType=> {
-    return {
-        id: value._id,
-        login: value.userName,
-        email: value.email,
-        createdAt: value.createdAt,
+    async userMapper (value: any): Promise<UserViewType>  {
+        return {
+            id: value._id,
+            login: value.userName,
+            email: value.email,
+            createdAt: value.createdAt,
+        }
     }
 }
+
+export const usersRepository = new UsersDbRepository()
+
