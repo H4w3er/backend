89de9852315d99bae079a8c8d3e9a14e4212031a
H4w3er <mr.gavrik378@list.ru>
2025-04-15 18:19:30 +0300

    recreated logic using classes

--- h_two/src/domain/users-service.ts
+++ h_two/src/domain/users-service.ts
@@ -1,20 +1,30 @@
 import {usersRepository} from "../repositories/users-db-repository";
-import {UserDbType} from "../db/user-type-db";
+import {UserDbTypeCommon} from "../db/user-type-db";
 import {ObjectId} from "mongodb";
 import bcrypt from 'bcrypt'
 
-
-export const usersService = {
+class UsersService{
     async createUser(login: string, password: string, email: string) {
         if (await usersRepository.findByLoginOrEmail(login)){
-            return 1
+            const error: object = {"errorsMessages": [
+                    {
+                        "message": "the login is not unique",
+                        "field": "login"
+                    }]}
+            return error
         }
         if (await usersRepository.findByLoginOrEmail(email)){
-            return 2
+            const error: object = {
+                "errorsMessages": [
+                    {
+                        "message": "the email address is not unique",
+                        "field": "email"
+                    }]}
+            return error
         }
         const passwordSalt = await bcrypt.genSalt(10)
         const passwordHash = await this.createHash(password, passwordSalt)
-        const newUser:UserDbType = {
+        const newUser:UserDbTypeCommon = {
             _id: new ObjectId(),
             userName: login,
             email,
@@ -28,27 +38,27 @@ export const usersService = {
             refreshTokenBlackList: []
         }
         return usersRepository.createUser(newUser)
-    },
+    }
     async deleteUser(id: string): Promise<boolean> {
         return usersRepository.deleteUser(id)
-    },
+    }
     async checkCredentials(loginOrEmail: string, password: string){
-         const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
+        const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
         if (!user) return false
-         const passwordHash = await this.createHash(password, user.passwordSalt)
-         if (user.passwordHash !== passwordHash){
-             return false
-         }
-         return user;
-    },
+        const passwordHash = await this.createHash(password, user.passwordSalt)
+        if (user.passwordHash !== passwordHash){
+            return false
+        }
+        return user;
+    }
     async createHash(password: string, salt: string){
         const bcrypt = require('bcrypt')
         const hash = await bcrypt.hash(password, salt)
         return hash;
-    },
+    }
     async findUserById(id: string){
         return usersRepository.findUserById(id)
-    },
+    }
     async isTokenAllowed(refreshToken: string, userId: string){
         const user = await usersRepository.findUserById(userId)
         if (!user) return false
@@ -56,13 +66,15 @@ export const usersService = {
             return false
         }
         return true
-    },
+    }
     async addToBlackList(refreshToken: string, userId: string){
         await usersRepository.addToBlackList(refreshToken, userId)
         return 0
-    },
+    }
     async addToBlackListAnother(refreshToken: string, userId: ObjectId){
         await usersRepository.addToBlackList(refreshToken, userId)
         return 0
     }
 }
+
+export const usersService = new UsersService()
