4d786cfd9c5c6846a398936882ebe71b48437794
H4w3er <mr.gavrik378@list.ru>
2025-04-24 20:13:15 +0300

    some change

--- h_two/src/repositories/users-db-repository.ts
+++ h_two/src/repositories/users-db-repository.ts
@@ -3,48 +3,59 @@ import {ObjectId} from "mongodb";
 import {v4 as uuidv4} from "uuid";
 import {UserViewType} from "../db/user-type-db";
 
-class UsersDbRepository{
+
+
+class UsersDbRepository {
     async createUser(newUser: any) {
         await userCollection.insertOne(newUser)
         return await this.userMapper(newUser);
     }
-    async deleteUser(id: string){
+
+    async deleteUser(id: string) {
         const objId = new ObjectId(id);
         const result = await userCollection.deleteOne({_id: objId})
         return result.deletedCount === 1;
     }
-    async findByLoginOrEmail(loginOrEmail: string){
+
+    async findByLoginOrEmail(loginOrEmail: string) {
         const user = await userCollection.findOne({$or: [{email: loginOrEmail}, {userName: loginOrEmail}]})
         return user
     }
-    async findUserById(id: string){
+
+    async findUserById(id: string) {
         const newId = new ObjectId(id)
         const user = await userCollection.findOne({_id: newId})
         return user
     }
-    async findUserByCode(code: string){
+
+    async findUserByCode(code: string) {
         const user = await userCollection.findOne({'emailConfirm.confCode': code})
         return user
     }
-    async updateUserByCode(code: string){
-        await userCollection.updateOne({'emailConfirm.confCode': code}, {$set:{'emailConfirm.isConfirmed': true}})
+
+    async updateUserByCode(code: string) {
+        await userCollection.updateOne({'emailConfirm.confCode': code}, {$set: {'emailConfirm.isConfirmed': true}})
     }
-    async updateUserCodeByCode(code: string){
+
+    async updateUserCodeByCode(code: string) {
         const newCode = uuidv4()
-        await userCollection.updateOne({'emailConfirm.confCode': code}, {$set:{'emailConfirm.confCode': newCode}})
+        await userCollection.updateOne({'emailConfirm.confCode': code}, {$set: {'emailConfirm.confCode': newCode}})
         return newCode
     }
-    async addToBlackList(refreshToken: string, userId: string | ObjectId){
-        await userCollection.updateOne({_id: new ObjectId(userId)}, {$push:{refreshTokenBlackList: refreshToken}})
+
+    async addToBlackList(refreshToken: string, userId: string | ObjectId) {
+        await userCollection.updateOne({_id: new ObjectId(userId)}, {$push: {refreshTokenBlackList: refreshToken}})
         return true
     }
-    async userMapper (value: any): Promise<UserViewType>  {
-        return {
-            id: value._id,
-            login: value.userName,
-            email: value.email,
-            createdAt: value.createdAt,
-        }
+
+    async userMapper(value: any): Promise<UserViewType> {
+        const user = new UserViewType(
+            value._id,
+            value.userName,
+            value.email,
+            value.createdAt
+        )
+        return user
     }
 }
 
