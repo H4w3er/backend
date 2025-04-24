e5574003d330f0a1b6e1cf43b183c0a0d3f282f9
H4w3er <mr.gavrik378@list.ru>
2025-04-24 20:08:35 +0300

    removed types and added classes

--- h_two/src/db/user-type-db.ts
+++ h_two/src/db/user-type-db.ts
@@ -1,22 +1,29 @@
 import {ObjectId} from "mongodb";
 
 
-export type UserDbTypeCommon = {
-    "_id": ObjectId,
-    "userName": string,
-    "email": string,
-    "passwordHash": string,
-    "passwordSalt": string,
-    "createdAt": string,
-    "emailConfirm":{
-        "confCode": string,
-        "isConfirmed": boolean
-    }
-    "refreshTokenBlackList": Array<string>
+export class UserDbTypeCommon {
+    constructor(public _id: ObjectId,
+        public userName: string,
+        public email: string,
+        public passwordHash: string,
+        public passwordSalt: string,
+        public createdAt: string,
+        public emailConfirm: {
+             confCode: string,
+             isConfirmed: boolean
+        },
+        public refreshTokenBlackList: Array<string>
+
+    )
+    {}
 }
-export type UserViewType = {
-    id: ObjectId
-    login: string
-    email: string
-    createdAt: string,
+
+export class UserViewType {
+    constructor(public id: ObjectId,
+                public login: string,
+                public email: string,
+                public createdAt: string
+    ) {
+
+    }
 }
