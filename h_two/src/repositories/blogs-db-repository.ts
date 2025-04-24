13e8741ddeed222f0b31d212b3ace15163c9fbe3
H4w3er <mr.gavrik378@list.ru>
2025-04-24 20:12:34 +0300

    recreated logic using classes

--- h_two/src/repositories/blogs-db-repository.ts
+++ h_two/src/repositories/blogs-db-repository.ts
@@ -7,23 +7,31 @@ import {
 } from "../db/mongo-db";
 import {ObjectId} from "mongodb";
 
-
-export const blogsRepository = {
-    async createBlog(newBlog: any){
+class BlogsDbRepository {
+    async createBlog(newBlog: any) {
         const idOfNewBlog = await blogCollection.insertOne(newBlog)
         return idOfNewBlog.insertedId;
-    },
-    async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean>{
+    }
+
+    async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
         const objId = new ObjectId(id);
-        const result = await blogCollection.updateOne({_id: objId}, {$set:{name: name, description: description, websiteUrl: websiteUrl}})
+        const result = await blogCollection.updateOne({_id: objId}, {
+            $set: {
+                name: name,
+                description: description,
+                websiteUrl: websiteUrl
+            }
+        })
         return result.matchedCount === 1;
-    },
-    async deleteBlog(id: string): Promise<boolean>{
+    }
+
+    async deleteBlog(id: string): Promise<boolean> {
         const objId = new ObjectId(id);
         const result = await blogCollection.deleteOne({_id: objId})
         return result.deletedCount === 1;
-    },
-    async deleteAll(): Promise<boolean>{
+    }
+
+    async deleteAll(): Promise<boolean> {
         await blogCollection.deleteMany()
         await postCollection.deleteMany()
         await userCollection.deleteMany()
@@ -31,10 +39,13 @@ export const blogsRepository = {
         await refreshTokenCollection.deleteMany()
         await requestsToApiCollection.deleteMany()
         return true;
-    },
-    async isBlog(blogId: string): Promise<boolean>{
+    }
+
+    async isBlog(blogId: string): Promise<boolean> {
         const id = new ObjectId(blogId)
         const result = await blogCollection.countDocuments({_id: id})
         return result === 1;
     }
 }
+
+export const blogsDbRepository = new BlogsDbRepository()
