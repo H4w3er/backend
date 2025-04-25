4dbb901eac5aca4129901fbfc46cfa444695f014
H4w3er <mr.gavrik378@list.ru>
2025-04-25 18:18:06 +0300

    recreated logic using classes with object dependencies

--- h_two/src/domain/securityDevices-service.ts
+++ h_two/src/domain/securityDevices-service.ts
@@ -1,41 +1,48 @@
-import {securityDevicesDbRepository} from "../repositories/securityDevices-db-repository";
+import {SecurityDevicesDbRepository} from "../repositories/securityDevices-db-repository";
 import {ObjectId} from "mongodb";
-import {usersService} from "./users-service";
+import {UsersService} from "./users-service";
 
-export const securityDevicesService = {
+export class SecurityDevicesService {
+    securityDevicesDbRepository: SecurityDevicesDbRepository
+    usersService: UsersService
+    constructor() {
+        this.securityDevicesDbRepository = new SecurityDevicesDbRepository()
+        this.usersService = new UsersService()
+    }
+
     async getActiveSessions(userId: ObjectId){
-        return await securityDevicesDbRepository.getActiveSessions(userId)
-    },
+        return await this.securityDevicesDbRepository.getActiveSessions(userId)
+    }
     async addNewSession(ip:string, title: string = 'noTitle', deviceId: string, userId: ObjectId, refreshToken: string){
         const issuedAt = new Date()
         const lastActiveDate = new Date().toISOString()
         const validUntil = new Date(new Date().setSeconds(new Date().getSeconds() + 20))
-        await securityDevicesDbRepository.addNewSession(ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId, refreshToken)
-    },
+        await this.securityDevicesDbRepository.addNewSession(ip, title, lastActiveDate, deviceId, issuedAt, validUntil, userId, refreshToken)
+    }
     async deleteAllOther(userId: string, deviceId: string){
-        const otherSessions = await securityDevicesDbRepository.getSessionsByUserId(userId)
+        const otherSessions = await this.securityDevicesDbRepository.getSessionsByUserId(userId)
         for (let i = 0; i<otherSessions.length; i++){
             if (otherSessions[i].deviceId != deviceId) {
-                await usersService.addToBlackListAnother(otherSessions[i].refreshToken, otherSessions[i].userId)
+                await this.usersService.addToBlackListAnother(otherSessions[i].refreshToken, otherSessions[i].userId)
             }
         }
-        await securityDevicesDbRepository.deleteAllOther(userId, deviceId)
-    },
+        await this.securityDevicesDbRepository.deleteAllOther(userId, deviceId)
+    }
     async deleteSessionByDeviceId(deviceId: string, userId: ObjectId){
-        const session = await securityDevicesDbRepository.getSessionsByDeviceId(deviceId)
+        const session = await this.securityDevicesDbRepository.getSessionsByDeviceId(deviceId)
         if (!session) return 'notFound'
         else if (session?.userId!=userId) return 'accessRejected'
         else {
-            const deleted = await securityDevicesDbRepository.deleteSessionByDeviceId(deviceId)
+            const deleted = await this.securityDevicesDbRepository.deleteSessionByDeviceId(deviceId)
             if (deleted.deletedCount === 0) return 'notFound'
             else {
-                await usersService.addToBlackListAnother(session.refreshToken, session.userId)
+                await this.usersService.addToBlackListAnother(session.refreshToken, session.userId)
                 return 'successDelete'
             }
         }
-    },
+    }
     async sessionUpdate(userId: ObjectId, deviceId: string, refreshToken: string){
         const date = new Date().toISOString()
-        await securityDevicesDbRepository.sessionUpdate(userId, deviceId, refreshToken, date)
+        await this.securityDevicesDbRepository.sessionUpdate(userId, deviceId, refreshToken, date)
     }
 }
