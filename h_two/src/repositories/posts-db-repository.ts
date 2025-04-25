fa6f12f64e0dd1414de9af5054b731b8c5bb271b
H4w3er <mr.gavrik378@list.ru>
2025-04-25 18:14:08 +0300

    recreated logic using classes with object dependencies

--- h_two/src/repositories/posts-db-repository.ts
+++ h_two/src/repositories/posts-db-repository.ts
@@ -1,16 +1,16 @@
 import {postCollection} from "../db/mongo-db";
 import {ObjectId} from "mongodb";
 
-export const postsRepository = {
+export class PostsDbRepository {
     async createPost(post: any){
         const newPostId = await postCollection.insertOne(post)
         return newPostId.insertedId;
-    },
+    }
     async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean>{
         const objId = new ObjectId(id);
         const result = await postCollection.updateOne({_id: objId}, {$set:{title: title, shortDescription: shortDescription, content: content, blogId: blogId}})
         return result.matchedCount === 1;
-    },
+    }
     async deletePost(id: string): Promise<boolean>{
         const objId = new ObjectId(id);
         const result = await postCollection.deleteOne({_id: objId})
