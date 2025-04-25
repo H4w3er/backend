d691bd348f462dfe4b06b4b00490a50e42b7a81e
H4w3er <mr.gavrik378@list.ru>
2025-04-25 17:35:00 +0300

    recreated logic using classes with object dependencies

--- h_two/src/routers/comments-router.ts
+++ h_two/src/routers/comments-router.ts
@@ -1,36 +1,41 @@
-import {Router} from "express";
+import {Router, Request, Response} from "express";
 import {authBearerMiddleware} from "../middlewares/authBearerMiddleware";
-import {commentsService} from "../domain/comments-service";
+import {CommentsService} from "../domain/comments-service";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 import {contentCommentValidation} from "../middlewares/comments-validation";
 
 export const commentsRouter = Router()
 
-commentsRouter.get('/:id',
-    async (req, res) => {
-        const comment = await commentsService.getCommentById(req.params.id)
+class CommentsController {
+    commentsService: CommentsService
+    constructor() {
+        this.commentsService = new CommentsService()
+    }
+    async getCommentById(req: Request, res: Response){
+        const comment = await this.commentsService.getCommentById(req.params.id)
         if (!comment) {
             res.sendStatus(404)
         } else res.status(200).send(comment)
-})
-
-commentsRouter.put('/:commentId',
-    authBearerMiddleware,
-    contentCommentValidation,
-    inputValidationMiddleware,
-    async (req,res) => {
-        const newComment = await commentsService.updateCommentById(req.params.commentId, req.body.content, req.user!._id)
+    }
+    async updateComment(req: Request, res: Response){
+        const newComment = await this.commentsService.updateCommentById(req.params.commentId, req.body.content, req.user!._id)
         if (!newComment) res.sendStatus(404)
-        else if (newComment===1) res.sendStatus(403)
+        else if (newComment === 1) res.sendStatus(403)
         else res.sendStatus(204)
-})
-
-commentsRouter.delete('/:id',
-    authBearerMiddleware,
-    async (req,res) => {
-        const newComment = await commentsService.deleteCommentById(req.params.id, req.user!._id)
+    }
+    async deleteComment(req: Request, res: Response){
+        const newComment = await this.commentsService.deleteCommentById(req.params.id, req.user!._id)
         if (!newComment) res.sendStatus(404)
-        else if (newComment===1) res.sendStatus(403)
+        else if (newComment === 1) res.sendStatus(403)
         else res.sendStatus(204)
-    })
+    }
+}
+
+const commentsController = new CommentsController()
+
+commentsRouter.get('/:id', commentsController.getCommentById.bind(commentsController))
+
+commentsRouter.put('/:commentId', authBearerMiddleware, contentCommentValidation, inputValidationMiddleware, commentsController.updateComment.bind(commentsController))
+
+commentsRouter.delete('/:id', authBearerMiddleware, commentsController.deleteComment.bind(commentsController))
 
