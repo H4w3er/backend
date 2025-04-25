6cadb5b4b328810cf750a8d7496f7ceb8a4d5536
H4w3er <mr.gavrik378@list.ru>
2025-04-25 18:13:59 +0300

    recreated logic using classes with object dependencies

--- h_two/src/repositories/posts-db-query-repository.ts
+++ h_two/src/repositories/posts-db-query-repository.ts
@@ -2,78 +2,77 @@ import {postCollection} from "../db/mongo-db";
 import {PostDbType} from "../db/posts-type-db";
 import {ObjectId} from "mongodb";
 
-const postMapper = (value: any) => {
-    if (value) {
-        return {
-            id: value._id,
-            title: value.title,
-            shortDescription: value.shortDescription,
-            content: value.content,
-            blogId: value.blogId,
-            blogName: value.blogName,
-            createdAt: value.createdAt
-        };
-    } else return null;
-}
-const postFilterForBlog = async (blogId: string, sortBy: string = "createdAt", sortDirection: any = 'desc', pageNumber:number=1, pageSize:number=10) => {
-    try {
-        // собственно запрос в бд (может быть вынесено во вспомогательный метод)
-        const items = await postCollection
-            .find({blogId: blogId})
-            .sort(sortBy, sortDirection)
-            .skip((pageNumber - 1) * pageSize)
-            .limit(Number(pageSize))
-            .toArray() as any[]
-        const totalCount = await postCollection.countDocuments({blogId: blogId})
-
-        // формирование ответа в нужном формате (может быть вынесено во вспомогательный метод)
-        return {
-            pagesCount: Math.ceil(totalCount / pageSize),
-            page: Number(pageNumber),
-            pageSize: Number(pageSize),
-            totalCount: totalCount,
-            items: items.map(value => postMapper(value))
-        }
-    } catch (e) {
-        console.log(e)
-        return {error: 'some error'}
+export class PostsDbQueryRepository {
+    async postMapper (value: any) {
+        if (value) {
+            return {
+                id: value._id,
+                title: value.title,
+                shortDescription: value.shortDescription,
+                content: value.content,
+                blogId: value.blogId,
+                blogName: value.blogName,
+                createdAt: value.createdAt
+            };
+        } else return null;
     }
-}
-const postFilter = async (sortBy: string = "createdAt", sortDirection: any = 'desc', pageNumber:number=1, pageSize:number=10) => {
-    try {
-        // собственно запрос в бд (может быть вынесено во вспомогательный метод)
-        const items = await postCollection
-            .find({})
-            .sort(sortBy, sortDirection)
-            .skip((pageNumber - 1) * pageSize)
-            .limit(Number(pageSize))
-            .toArray() as any[]
-        const totalCount = await postCollection.countDocuments()
+    async postFilterForBlog (blogId: string, sortBy: string = "createdAt", sortDirection: any = 'desc', pageNumber:number=1, pageSize:number=10) {
+        try {
+            // собственно запрос в бд (может быть вынесено во вспомогательный метод)
+            const items = await postCollection
+                .find({blogId: blogId})
+                .sort(sortBy, sortDirection)
+                .skip((pageNumber - 1) * pageSize)
+                .limit(Number(pageSize))
+                .toArray() as any[]
+            const totalCount = await postCollection.countDocuments({blogId: blogId})
 
-        // формирование ответа в нужном формате (может быть вынесено во вспомогательный метод)
-        return {
-            pagesCount: Math.ceil(totalCount / pageSize),
-            page: Number(pageNumber),
-            pageSize: Number(pageSize),
-            totalCount: totalCount,
-            items: items.map(value => postMapper(value))
+            // формирование ответа в нужном формате (может быть вынесено во вспомогательный метод)
+            return {
+                pagesCount: Math.ceil(totalCount / pageSize),
+                page: Number(pageNumber),
+                pageSize: Number(pageSize),
+                totalCount: totalCount,
+                items: items.map(value => this.postMapper(value))
+            }
+        } catch (e) {
+            console.log(e)
+            return {error: 'some error'}
         }
-    } catch (e) {
-        console.log(e)
-        return {error: 'some error'}
     }
-}
+    async postFilter (sortBy: string = "createdAt", sortDirection: any = 'desc', pageNumber:number=1, pageSize:number=10) {
+        try {
+            // собственно запрос в бд (может быть вынесено во вспомогательный метод)
+            const items = await postCollection
+                .find({})
+                .sort(sortBy, sortDirection)
+                .skip((pageNumber - 1) * pageSize)
+                .limit(Number(pageSize))
+                .toArray() as any[]
+            const totalCount = await postCollection.countDocuments()
 
-export const postQueryRepository = {
+            // формирование ответа в нужном формате (может быть вынесено во вспомогательный метод)
+            return {
+                pagesCount: Math.ceil(totalCount / pageSize),
+                page: Number(pageNumber),
+                pageSize: Number(pageSize),
+                totalCount: totalCount,
+                items: items.map(value => this.postMapper(value))
+            }
+        } catch (e) {
+            console.log(e)
+            return {error: 'some error'}
+        }
+    }
     async findPosts(sortBy: any, sortDirection: any, pageNumber: number, pageSize: number){
-        return postFilter(sortBy, sortDirection, pageNumber, pageSize)
-    },
+        return this.postFilter(sortBy, sortDirection, pageNumber, pageSize)
+    }
     async findPostById(id: string): Promise<PostDbType | null>{
         const objId = new ObjectId(id);
         const post = await postCollection.findOne({_id: objId})
-        return postMapper(post)
-    },
+        return this.postMapper(post)
+    }
     async postsForBlog(blogId: string, sortBy: any, sortDirection: any, pageNumber: number, pageSize: number){
-        return postFilterForBlog(blogId, sortBy, sortDirection, pageNumber, pageSize)
+        return this.postFilterForBlog(blogId, sortBy, sortDirection, pageNumber, pageSize)
     }
 }
