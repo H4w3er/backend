6bdd30f9cd5c1aaca00717b097bc9d75ba4da943
H4w3er <mr.gavrik378@list.ru>
2025-04-25 18:18:12 +0300

    recreated logic using classes with object dependencies

--- h_two/src/domain/users-service.ts
+++ h_two/src/domain/users-service.ts
@@ -1,11 +1,15 @@
-import {usersRepository} from "../repositories/users-db-repository";
+import {UsersDbRepository} from "../repositories/users-db-repository";
 import {UserDbTypeCommon} from "../db/user-type-db";
 import {ObjectId} from "mongodb";
 import bcrypt from 'bcrypt'
 
-class UsersService{
+export class UsersService{
+    usersDbRepository: UsersDbRepository
+    constructor() {
+        this.usersDbRepository = new UsersDbRepository()
+    }
     async createUser(login: string, password: string, email: string) {
-        if (await usersRepository.findByLoginOrEmail(login)){
+        if (await this.usersDbRepository.findByLoginOrEmail(login)){
             const error: object = {"errorsMessages": [
                     {
                         "message": "the login is not unique",
@@ -13,7 +17,7 @@ class UsersService{
                     }]}
             return error
         }
-        if (await usersRepository.findByLoginOrEmail(email)){
+        if (await this.usersDbRepository.findByLoginOrEmail(email)){
             const error: object = {
                 "errorsMessages": [
                     {
@@ -37,13 +41,13 @@ class UsersService{
             },
             []
     )
-        return usersRepository.createUser(newUser)
+        return this.usersDbRepository.createUser(newUser)
     }
     async deleteUser(id: string): Promise<boolean> {
-        return usersRepository.deleteUser(id)
+        return this.usersDbRepository.deleteUser(id)
     }
     async checkCredentials(loginOrEmail: string, password: string){
-        const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
+        const user = await this.usersDbRepository.findByLoginOrEmail(loginOrEmail)
         if (!user) return false
         const passwordHash = await this.createHash(password, user.passwordSalt)
         if (user.passwordHash !== passwordHash){
@@ -57,10 +61,10 @@ class UsersService{
         return hash;
     }
     async findUserById(id: string){
-        return usersRepository.findUserById(id)
+        return this.usersDbRepository.findUserById(id)
     }
     async isTokenAllowed(refreshToken: string, userId: string){
-        const user = await usersRepository.findUserById(userId)
+        const user = await this.usersDbRepository.findUserById(userId)
         if (!user) return false
         if (user?.refreshTokenBlackList.includes(refreshToken)){
             return false
@@ -68,13 +72,11 @@ class UsersService{
         return true
     }
     async addToBlackList(refreshToken: string, userId: string){
-        await usersRepository.addToBlackList(refreshToken, userId)
+        await this.usersDbRepository.addToBlackList(refreshToken, userId)
         return 0
     }
     async addToBlackListAnother(refreshToken: string, userId: ObjectId){
-        await usersRepository.addToBlackList(refreshToken, userId)
+        await this.usersDbRepository.addToBlackList(refreshToken, userId)
         return 0
     }
 }
-
-export const usersService = new UsersService()
