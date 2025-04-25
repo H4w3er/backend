3b49fd2e23b6807d196d406bd1d264efd1985069
H4w3er <mr.gavrik378@list.ru>
2025-04-25 18:17:26 +0300

    recreated logic using classes with object dependencies

--- h_two/src/domain/auth-service.ts
+++ h_two/src/domain/auth-service.ts
@@ -1,13 +1,19 @@
-import {usersRepository} from "../repositories/users-db-repository";
+import {UsersDbRepository} from "../repositories/users-db-repository";
 import {UserDbTypeCommon} from "../db/user-type-db";
 import {ObjectId} from "mongodb";
 import bcrypt from 'bcrypt'
 import { v4 as uuidv4 } from 'uuid';
-import {emailAdapter} from "../adapters/emailAdapter";
+import {EmailAdapter} from "../adapters/emailAdapter";
 
-export const authService = {
+export class AuthService {
+    usersDbRepository: UsersDbRepository
+    emailAdapter: EmailAdapter
+    constructor() {
+        this.usersDbRepository = new UsersDbRepository()
+        this.emailAdapter = new EmailAdapter()
+    }
     async createUser(login: string, password: string, email: string) {
-        if (await usersRepository.findByLoginOrEmail(login)){
+        if (await this.usersDbRepository.findByLoginOrEmail(login)){
             const error: object = {"errorsMessages": [
                     {
                         "message": "the login is not unique",
@@ -15,7 +21,7 @@ export const authService = {
                     }]}
             return error
         }
-        if (await usersRepository.findByLoginOrEmail(email)){
+        if (await this.usersDbRepository.findByLoginOrEmail(email)){
             const error: object = {"errorsMessages":[
                     {
                         "message": "the email address is not unique",
@@ -38,21 +44,21 @@ export const authService = {
             },
             []
     )
-        await emailAdapter.sendEmail(email, newUser.emailConfirm.confCode)
-        return usersRepository.createUser(newUser)
-    },
+        await this.emailAdapter.sendEmail(email, newUser.emailConfirm.confCode)
+        return this.usersDbRepository.createUser(newUser)
+    }
     async createHash(password: string, salt: string){
         const hash = await bcrypt.hash(password, salt)
         return hash;
-    },
+    }
     async sendConfirmationLetter(email: string){
-        const user = await usersRepository.findByLoginOrEmail(email)
+        const user = await this.usersDbRepository.findByLoginOrEmail(email)
         if (!user){
             return null
         }
         if (user.emailConfirm.isConfirmed) return null
-        const newCode = await usersRepository.updateUserCodeByCode(user.emailConfirm.confCode)
-        await emailAdapter.sendEmail(email, newCode)
+        const newCode = await this.usersDbRepository.updateUserCodeByCode(user.emailConfirm.confCode)
+        await this.emailAdapter.sendEmail(email, newCode)
         return true
     }
 }
