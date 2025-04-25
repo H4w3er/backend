746c8399879bdc1b70b0e17818838232b105c1ee
H4w3er <mr.gavrik378@list.ru>
2025-04-25 17:34:32 +0300

    recreated logic using classes with object dependencies

--- h_two/src/routers/auth-router.ts
+++ h_two/src/routers/auth-router.ts
@@ -1,91 +1,121 @@
-import {Router} from "express";
-import {usersService} from "../domain/users-service";
-import {jwtService} from "../application/jwt-service";
+import {Router, Request, Response} from "express";
+import {UsersService} from "../domain/users-service";
+import {JwtService} from "../application/jwt-service";
 import {authBearerMiddleware} from "../middlewares/authBearerMiddleware";
-import {emailAdapter} from "../adapters/emailAdapter";
+import {EmailAdapter} from "../adapters/emailAdapter";
 import {emailValidation, loginValidation, passwordValidation} from "../middlewares/auth-validation";
-import {authService} from "../domain/auth-service";
+import {AuthService} from "../domain/auth-service";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 import {authRefreshMiddleware} from "../middlewares/authRefreshMiddleware";
-import {securityDevicesService} from "../domain/securityDevices-service";
+import {SecurityDevicesService} from "../domain/securityDevices-service";
 import {CRLChecking} from "../middlewares/CRL-cheking";
 import {UserViewType} from "../db/user-type-db";
 
 export const authRouter = Router({})
 
-authRouter.post('/login', CRLChecking, async (req,res) =>{
-    const checkUser = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
-    if (checkUser){
-        const token = await jwtService.createJWT(checkUser._id)
-        const refreshToken = await jwtService.createRefreshToken(checkUser._id, null)
-        const ip =  req.headers['x-forwarded-for'] as string
-        const title =  req.headers['user-agent'] as string
-        await securityDevicesService.addNewSession(ip, title, refreshToken.deviceId.toString(), checkUser._id, refreshToken.token)
+class AuthController{
+    usersService: UsersService
+    emailAdapter: EmailAdapter
+    authService: AuthService
+    securityDevicesService: SecurityDevicesService
+    jwtService: JwtService
+    constructor() {
+        this.usersService = new UsersService()
+        this.emailAdapter = new EmailAdapter()
+        this.authService = new AuthService()
+        this.securityDevicesService = new SecurityDevicesService()
+        this.jwtService = new JwtService()
+    }
 
-        res.cookie('refreshToken', refreshToken.token, {httpOnly: true, secure: true})
-        res.status(200).send({accessToken: token})
-    } else {
-        res.sendStatus(401)
-    }
-})
+    async login(req:Request, res: Response){
+        const checkUser = await this.usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
+        if (checkUser){
+            const token = await this.jwtService.createJWT(checkUser._id)
+            const refreshToken = await this.jwtService.createRefreshToken(checkUser._id, null)
+            const ip =  req.headers['x-forwarded-for'] as string
+            const title =  req.headers['user-agent'] as string
+            await this.securityDevicesService.addNewSession(ip, title, refreshToken.deviceId.toString(), checkUser._id, refreshToken.token)
 
-authRouter.get('/me', authBearerMiddleware, async (req, res) =>{
-    const thisUser = {
-        "email": req.user!.email,
-        "login": req.user!.userName,
-        "userId": req.user!._id,
+            res.cookie('refreshToken', refreshToken.token, {httpOnly: true, secure: true})
+            res.status(200).send({accessToken: token})
+        } else {
+            res.sendStatus(401)
+        }
     }
-    res.status(200).send(thisUser)
-})
-authRouter.post('/registration-email-resending', CRLChecking, emailValidation, inputValidationMiddleware, async (req, res) =>{
-    const result = await authService.sendConfirmationLetter(req.body.email)
-    if (result) res.sendStatus(204)
-    else res.status(400).send({"errorsMessages": [
-            {
-                "message": "something went wrong",
-                "field": "email"
-            }]})
-})
-authRouter.post('/registration', CRLChecking, loginValidation, emailValidation, passwordValidation, inputValidationMiddleware, async (req, res) =>{
-    const newUser: UserViewType | Object = await authService.createUser(req.body.login, req.body.password, req.body.email)
-    if('id' in newUser) {
-        res.sendStatus(204)
+    async infoMe(req:Request, res: Response){
+        const thisUser = {
+            "email": req.user!.email,
+            "login": req.user!.userName,
+            "userId": req.user!._id,
+        }
+        res.status(200).send(thisUser)
     }
-    else{
-        res.status(400).send(newUser)
+    async emailResending(req:Request, res: Response){
+        const result = await this.authService.sendConfirmationLetter(req.body.email)
+        if (result) res.sendStatus(204)
+        else res.status(400).send({"errorsMessages": [
+                {
+                    "message": "something went wrong",
+                    "field": "email"
+                }]})
     }
-})
-authRouter.post('/registration-confirmation', CRLChecking, async (req, res) =>{
-    const verify = await emailAdapter.checkCode(req.body.code)
-    if (verify) res.sendStatus(204)
-    else res.status(400).send({"errorsMessages": [
-            {
-                "message": "wrong code",
-                "field": "code"
-            }]})
-})
-authRouter.post('/refresh-token', authRefreshMiddleware, async (req, res) =>{
-    const refreshToken = req.cookies.refreshToken;
-    const verifiedToken = await jwtService.getIdFromToken(refreshToken)
-    if (!verifiedToken) {
-        res.sendStatus(401)
-    } else {
-        await usersService.addToBlackList(refreshToken, verifiedToken.userId)
-        const newToken = await jwtService.createJWT(verifiedToken.userId)
-        const newRefreshToken = await jwtService.createRefreshToken(verifiedToken.userId, verifiedToken.deviceId)
-        await securityDevicesService.sessionUpdate(verifiedToken.userId, verifiedToken.deviceId, newRefreshToken.token)
-        res.cookie('refreshToken', newRefreshToken.token, {httpOnly: true, secure: true})
-        res.status(200).send({accessToken: newToken})
+    async registration(req:Request, res: Response){
+        const newUser: UserViewType | Object = await this.authService.createUser(req.body.login, req.body.password, req.body.email)
+        if('id' in newUser) {
+            res.sendStatus(204)
+        }
+        else{
+            res.status(400).send(newUser)
+        }
     }
-})
-authRouter.post('/logout', authRefreshMiddleware, async(req, res)=>{
-    const refreshToken = req.cookies.refreshToken;
-    const verifiedToken = await jwtService.getIdFromToken(refreshToken)
-    if (!verifiedToken.userId) {
-        res.sendStatus(401)
-    } else {
-        await usersService.addToBlackList(refreshToken, verifiedToken.userId)
-        await securityDevicesService.deleteSessionByDeviceId(verifiedToken.deviceId, verifiedToken.userId)
-        res.sendStatus(204)
+    async registrationConfirmation(req:Request, res: Response){
+        const verify = await this.emailAdapter.checkCode(req.body.code)
+        if (verify) res.sendStatus(204)
+        else res.status(400).send({"errorsMessages": [
+                {
+                    "message": "wrong code",
+                    "field": "code"
+                }]})
     }
-})
+    async refreshToken(req:Request, res: Response){
+        const refreshToken = req.cookies.refreshToken;
+        const verifiedToken = await this.jwtService.getIdFromToken(refreshToken)
+        if (!verifiedToken) {
+            res.sendStatus(401)
+        } else {
+            await this.usersService.addToBlackList(refreshToken, verifiedToken.userId)
+            const newToken = await this.jwtService.createJWT(verifiedToken.userId)
+            const newRefreshToken = await this.jwtService.createRefreshToken(verifiedToken.userId, verifiedToken.deviceId)
+            await this.securityDevicesService.sessionUpdate(verifiedToken.userId, verifiedToken.deviceId, newRefreshToken.token)
+            res.cookie('refreshToken', newRefreshToken.token, {httpOnly: true, secure: true})
+            res.status(200).send({accessToken: newToken})
+        }
+    }
+    async logout(req:Request, res: Response){
+        const refreshToken = req.cookies.refreshToken;
+        const verifiedToken = await this.jwtService.getIdFromToken(refreshToken)
+        if (!verifiedToken.userId) {
+            res.sendStatus(401)
+        } else {
+            await this.usersService.addToBlackList(refreshToken, verifiedToken.userId)
+            await this.securityDevicesService.deleteSessionByDeviceId(verifiedToken.deviceId, verifiedToken.userId)
+            res.sendStatus(204)
+        }
+    }
+}
+
+const authController = new AuthController()
+
+authRouter.post('/login', CRLChecking, authController.login.bind(authController))
+
+authRouter.get('/me', authBearerMiddleware, authController.infoMe.bind(authController))
+
+authRouter.post('/registration-email-resending', CRLChecking, emailValidation, inputValidationMiddleware, authController.emailResending.bind(authController))
+
+authRouter.post('/registration', CRLChecking, loginValidation, emailValidation, passwordValidation, inputValidationMiddleware, authController.registration.bind(authController))
+
+authRouter.post('/registration-confirmation', CRLChecking, authController.registrationConfirmation.bind(authController))
+
+authRouter.post('/refresh-token', authRefreshMiddleware, authController.refreshToken.bind(authController))
+
+authRouter.post('/logout', authRefreshMiddleware, authController.logout.bind(authController))
