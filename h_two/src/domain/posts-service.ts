2f474847458ac6dad23258265ff2e2d48632aefd
H4w3er <mr.gavrik378@list.ru>
2025-04-25 18:17:59 +0300

    recreated logic using classes with object dependencies

--- h_two/src/domain/posts-service.ts
+++ h_two/src/domain/posts-service.ts
@@ -1,24 +1,30 @@
-import {postsRepository} from "../repositories/posts-db-repository";
-import {blogQueryRepository} from "../repositories/blogs-db-query-repository";
+import {PostsDbRepository} from "../repositories/posts-db-repository";
+import {BlogsDbQueryRepository} from "../repositories/blogs-db-query-repository";
 
 
-export const postsService = {
+export class PostsService {
+    postsDbRepository: PostsDbRepository
+    blogsDbQueryRepository: BlogsDbQueryRepository
+    constructor() {
+        this.postsDbRepository = new PostsDbRepository()
+        this.blogsDbQueryRepository = new BlogsDbQueryRepository()
+    }
     async createPost(title: string, shortDescription: string, content: string, blogId: string) {
         const newPost ={
             title: title,
             shortDescription: shortDescription,
             content: content,
             blogId: blogId,
-            blogName: (await blogQueryRepository.getBlogName(blogId)),
+            blogName: (await this.blogsDbQueryRepository.getBlogName(blogId)),
             createdAt: new Date().toISOString()
         }
-        return postsRepository.createPost(newPost)
-    },
+        return this.postsDbRepository.createPost(newPost)
+    }
     async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean> {
-        return postsRepository.updatePost(id, title, shortDescription, content, blogId)
-    },
+        return this.postsDbRepository.updatePost(id, title, shortDescription, content, blogId)
+    }
     async deletePost(id: string): Promise<boolean> {
-        return postsRepository.deletePost(id)
-    },
+        return this.postsDbRepository.deletePost(id)
+    }
 
 }
