4a3ed39e07483b47f3741a0852f2c7ca855748a1
H4w3er <mr.gavrik378@list.ru>
2025-04-25 17:34:46 +0300

    recreated logic using classes with object dependencies

--- h_two/src/routers/users-router.ts
+++ h_two/src/routers/users-router.ts
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import {Request, Response} from 'express'
-import {usersQueryRepository} from "../repositories/users-db-query-repository";
-import {usersService} from "../domain/users-service";
+import {UsersDbQueryRepository} from "../repositories/users-db-query-repository";
+import {UsersService} from "../domain/users-service";
 import {authMiddleware} from "../middlewares/authMiddleware";
 import {emailValidation, loginValidation, passwordValidation} from "../middlewares/users-validation";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
@@ -9,6 +9,13 @@ import {inputValidationMiddleware} from "../middlewares/input-validation-middlew
 export const usersRouter = Router({})
 
 class UsersController {
+    usersService: UsersService
+    usersDbQueryRepository: UsersDbQueryRepository
+    constructor() {
+        this.usersService = new UsersService()
+        this.usersDbQueryRepository= new UsersDbQueryRepository()
+    }
+
     async getUsers(req: Request, res: Response) {
         const sortBy = req.query.sortBy as string
         const sortDirection = req.query.sortDirection as string
@@ -16,14 +23,14 @@ class UsersController {
         const pageSize = req.query.pageSize as string
         const searchLoginTerm = req.query.searchLoginTerm as string
         const searchEmailTerm = req.query.searchEmailTerm as string
-        const users = await usersQueryRepository.getUsers(sortBy, sortDirection, pageNumber, pageSize, searchLoginTerm, searchEmailTerm)
+        const users = await this.usersDbQueryRepository.getUsers(sortBy, sortDirection, pageNumber, pageSize, searchLoginTerm, searchEmailTerm)
         res.status(200).send(users)
     }
     async createUser(req: Request, res: Response) {
         const login = req.body.login as string
         const password = req.body.password as string
         const email = req.body.email as string
-        const newUser = await usersService.createUser(login, password, email)
+        const newUser = await this.usersService.createUser(login, password, email)
         if('id' in newUser) {
             res.status(201).send(newUser)
         }
@@ -33,7 +40,7 @@ class UsersController {
     }
     async deleteUser(req: Request, res: Response) {
         const id = req.params.id as string
-        if (await usersService.deleteUser(id)) {
+        if (await this.usersService.deleteUser(id)) {
             res.sendStatus(204)
         } else {
             res.sendStatus(404)
@@ -43,6 +50,6 @@ class UsersController {
 
 const usersController = new UsersController()
 
-usersRouter.get('/', authMiddleware, usersController.getUsers )
-usersRouter.post('/', authMiddleware, loginValidation, passwordValidation, emailValidation, inputValidationMiddleware, usersController.createUser)
-usersRouter.delete('/:id', authMiddleware, usersController.deleteUser)
+usersRouter.get('/', authMiddleware, usersController.getUsers.bind(usersController))
+usersRouter.post('/', authMiddleware, loginValidation, passwordValidation, emailValidation, inputValidationMiddleware, usersController.createUser.bind(usersController))
+usersRouter.delete('/:id', authMiddleware, usersController.deleteUser.bind(usersController))
