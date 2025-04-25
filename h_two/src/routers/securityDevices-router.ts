d67cc0da48e152f2eaf957d019076d5c8758f170
H4w3er <mr.gavrik378@list.ru>
2025-04-25 17:35:06 +0300

    recreated logic using classes with object dependencies

--- h_two/src/routers/securityDevices-router.ts
+++ h_two/src/routers/securityDevices-router.ts
@@ -1,7 +1,7 @@
-import {Router} from "express";
+import {Router, Request, Response} from "express";
 import {authRefreshMiddleware} from "../middlewares/authRefreshMiddleware";
-import {securityDevicesService} from "../domain/securityDevices-service";
-import {jwtService} from "../application/jwt-service";
+import {SecurityDevicesService} from "../domain/securityDevices-service";
+import {JwtService} from "../application/jwt-service";
 
 export const securityDevicesRouter = Router({});
 
@@ -10,27 +10,42 @@ answersMap.set("accessRejected", 403)
 answersMap.set("notFound", 404)
 answersMap.set("successDelete", 204)
 
-securityDevicesRouter.get('/devices', authRefreshMiddleware, async (req, res) =>{
-    const refreshToken = req.cookies.refreshToken;
-    const verifiedToken = await jwtService.getIdFromToken(refreshToken)
-    await securityDevicesService.sessionUpdate(verifiedToken.userId, verifiedToken.deviceId, refreshToken)
+class SecurityDevicesController {
+    securityDevicesService: SecurityDevicesService
+    jwtService: JwtService
 
-    const sessions = await securityDevicesService.getActiveSessions(verifiedToken.userId)
-    res.status(200).send(sessions)
-})
+    constructor() {
+        this.securityDevicesService = new SecurityDevicesService()
+        this.jwtService = new JwtService()
+    }
 
-securityDevicesRouter.delete('/devices', authRefreshMiddleware, async (req, res) =>{
-    const refreshToken = req.cookies.refreshToken;
-    const verifiedToken = await jwtService.getIdFromToken(refreshToken)
-    await securityDevicesService.deleteAllOther(verifiedToken.userId, verifiedToken.deviceId)
-    res.sendStatus(204)
-})
+    async getSessions(req: Request, res: Response) {
+        const refreshToken = req.cookies.refreshToken;
+        const verifiedToken = await this.jwtService.getIdFromToken(refreshToken)
+        await this.securityDevicesService.sessionUpdate(verifiedToken.userId, verifiedToken.deviceId, refreshToken)
 
-securityDevicesRouter.delete('/devices/:id', authRefreshMiddleware, async (req, res) =>{
-    const refreshToken = req.cookies.refreshToken;
-    const deviceId = req.params.id
-    const userIdFromToken = await jwtService.getIdFromToken(refreshToken)
-    const deleted = await securityDevicesService.deleteSessionByDeviceId(deviceId, userIdFromToken.userId)
+        const sessions = await this.securityDevicesService.getActiveSessions(verifiedToken.userId)
+        res.status(200).send(sessions)
+    }
+    async deleteOtherSessions(req: Request, res: Response) {
+        const refreshToken = req.cookies.refreshToken;
+        const verifiedToken = await this.jwtService.getIdFromToken(refreshToken)
+        await this.securityDevicesService.deleteAllOther(verifiedToken.userId, verifiedToken.deviceId)
+        res.sendStatus(204)
+    }
+    async deleteSessionById(req: Request, res: Response){
+        const refreshToken = req.cookies.refreshToken;
+        const deviceId = req.params.id
+        const userIdFromToken = await this.jwtService.getIdFromToken(refreshToken)
+        const deleted = await this.securityDevicesService.deleteSessionByDeviceId(deviceId, userIdFromToken.userId)
+        res.sendStatus(answersMap.get(deleted))
+    }
+}
 
-    res.sendStatus(answersMap.get(deleted))
-})
+const securityDevicesController = new SecurityDevicesController()
+
+securityDevicesRouter.get('/devices', authRefreshMiddleware, securityDevicesController.getSessions.bind(securityDevicesController))
+
+securityDevicesRouter.delete('/devices', authRefreshMiddleware, securityDevicesController.deleteOtherSessions.bind(securityDevicesController))
+
+securityDevicesRouter.delete('/devices/:id', authRefreshMiddleware, securityDevicesController.deleteSessionById.bind(securityDevicesController))
