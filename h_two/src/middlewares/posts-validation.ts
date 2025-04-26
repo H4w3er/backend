de5f775db8ba8fe67a61099af7ccbc7017a69d26
H4w3er <mr.gavrik378@list.ru>
2025-04-26 15:47:53 +0300

    removed validation

--- h_two/src/middlewares/posts-validation.ts
+++ h_two/src/middlewares/posts-validation.ts
@@ -1,13 +1,10 @@
 import {body} from "express-validator";
-import {BlogsService} from "../domain/blogs-service";
-
-const blogsService = new BlogsService()
 
 export const titleValidation = body('title').trim().isLength({min:1, max:30}).withMessage("Title should be more than 1 and less than 30");
 export const shortDescriptionValidation = body('shortDescription').isLength({min:1, max:100}).withMessage("shortDescription should be more than 1 and less than 100");
 export const contentValidation = body('content').trim().isLength({min:1, max:1000}).withMessage("Content should be more than 1 and less than 1000")
-export const blogIdValidation= body('blogId').custom(async value => {
+/*export const blogIdValidation= body('blogId').custom(async value => {
     const check = await blogsService.isBlog(value);
     if (!check) throw new Error()
     return true;
-}).withMessage("Blog not found")
+}).withMessage("Blog not found")*/
