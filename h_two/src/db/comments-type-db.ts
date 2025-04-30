d5648b858285d71ba1627665624658156bcfd5c7
H4w3er <mr.gavrik378@list.ru>
2025-04-30 21:03:06 +0300

    comments type recreated in class and added schema with model to integrate comments in mongoose

--- h_two/src/db/comments-type-db.ts
+++ h_two/src/db/comments-type-db.ts
@@ -1,12 +1,38 @@
 import {ObjectId} from "mongodb";
+import mongoose from "mongoose";
 
-export type CommentsDbType = {
-    "_id": ObjectId,
-    "content": string,
-    "commentatorInfo": {
-        "userId": string,
-        "userLogin": string
-    },
-    "createdAt": string,
-    "postId": ObjectId
+export class CommentsDbTypeCommon {
+    constructor(
+        public _id: ObjectId,
+        public content: string,
+        public commentatorInfo: {
+            userId: ObjectId,
+            userLogin: string
+        },
+        public createdAt: string,
+        public postId: ObjectId,
+        public likesInfo: {
+            likesCount: number,
+            dislikesCount: number,
+            myStatus: string
+        }
+    ) {
+    }
 }
+
+export const CommentsSchema = new mongoose.Schema<CommentsDbTypeCommon>({
+    _id: {type: ObjectId, require: true},
+    content: {type: String, require: true},
+    commentatorInfo: {
+        userId: ObjectId,
+        userLogin: String
+    },
+    createdAt: {type: String, require: true},
+    postId: {type: ObjectId, require: true},
+    likesInfo: {
+        likesCount: Number,
+        dislikesCount: Number,
+        myStatus: ['None', 'Like', 'Dislike']
+    }
+})
+export const CommentsModel = mongoose.model<CommentsDbTypeCommon>('comments', CommentsSchema)
