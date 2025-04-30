061a0bba094d522dea7d861e3d83dff970ac7906
H4w3er <mr.gavrik378@list.ru>
2025-04-30 21:03:27 +0300

    changed type of comments

--- h_two/src/db/mongo-db.ts
+++ h_two/src/db/mongo-db.ts
@@ -3,11 +3,11 @@ import {Collection, Db, MongoClient} from "mongodb";
 import {BlogDbType} from "./blogs-type-db";
 import {PostDbType} from "./posts-type-db";
 import {UserDbTypeCommon} from "./user-type-db";
-import {CommentsDbType} from "./comments-type-db";
 import {refreshTokenDb} from "./refresh-Token-db";
 import {requestsToApi} from "./requests-to-api-type-db";
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import {CommentsDbTypeCommon} from "./comments-type-db";
 
 dotenv.config()
 
@@ -34,7 +34,7 @@ export const db: Db = client.db(SETTINGS.DB_NAME);
 export const blogCollection: Collection<BlogDbType> = db.collection<BlogDbType>(SETTINGS.PATH.BLOGS)
 export const postCollection: Collection<PostDbType> = db.collection<PostDbType>(SETTINGS.PATH.POSTS)
 export const userCollection: Collection<UserDbTypeCommon> = db.collection<UserDbTypeCommon>(SETTINGS.PATH.USERS)
-export const commentsCollection: Collection<CommentsDbType> = db.collection<CommentsDbType>(SETTINGS.PATH.COMMENTS)
+export const commentsCollection: Collection<CommentsDbTypeCommon> = db.collection<CommentsDbTypeCommon>(SETTINGS.PATH.COMMENTS)
 export const refreshTokenCollection: Collection<refreshTokenDb> = db.collection<refreshTokenDb>(SETTINGS.PATH.SECURITYDEVICES)
 export const requestsToApiCollection: Collection<requestsToApi> = db.collection<requestsToApi>(SETTINGS.PATH.REQUESTSTOAPI)
 
