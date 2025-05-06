86b71bd9d334ef569948bc51303fe28c3704d78d
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:34:00 +0300

    added class, schema and model for sessions restriction

--- h_two/src/db/refresh-Token-db.ts
+++ h_two/src/db/refresh-Token-db.ts
@@ -1,12 +1,29 @@
 import {ObjectId} from "mongodb";
+import mongoose from "mongoose";
 
-export type refreshTokenDb = {
-    ip: string
-    title: string | 'noTitle'
-    lastActiveDate: string
-    deviceId: string
-    issuedAt: Date
-    validUntil: Date
-    userId: ObjectId
-    refreshToken: string
+export class RefreshTokenDb {
+    constructor(
+        public ip: string,
+        public title: string,
+        public lastActiveDate: string,
+        public deviceId: string,
+        public issuedAt: Date,
+        public validUntil: Date,
+        public userId: ObjectId,
+        public refreshToken: string
+    ) {
+
+    }
 }
+
+export const RefreshTokenDbSchema = new mongoose.Schema<RefreshTokenDb>({
+    ip: {type: String, require: true},
+    title: {type: String, require: true},
+    lastActiveDate: {type: String, require: true},
+    deviceId: {type: String, require: true},
+    issuedAt: {type: Date, require: true},
+    validUntil: {type: Date, require: true},
+    userId: {type: ObjectId, require: true},
+    refreshToken: {type: String, require: true}
+})
+export const RefreshTokenDbModel = mongoose.model<RefreshTokenDb>('refreshTokens', RefreshTokenDbSchema)
