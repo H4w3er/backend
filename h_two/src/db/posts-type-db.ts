8bd48b8f253c33d80f7a39dc49f3b028cde61fcf
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:33:31 +0300

    added class, schema and model for posts

--- h_two/src/db/posts-type-db.ts
+++ h_two/src/db/posts-type-db.ts
@@ -1,8 +1,27 @@
-export type PostDbType = {
-    title: string // max 30
-    shortDescription: string // max 100
-    content: string // max 1000
-    blogId: string // valid
-    blogName: string
-    createdAt: string
+import mongoose from "mongoose";
+import {ObjectId} from "mongodb";
+
+export class PostDbType {
+    constructor(
+        public _id: ObjectId,
+        public title: string,
+        public shortDescription: string,
+        public content: string,
+        public blogId: string,
+        public blogName: string,
+        public createdAt: string
+    ) {
+
+    }
 }
+
+export const PostDbTypeSchema = new mongoose.Schema<PostDbType>({
+    _id: {type: ObjectId, require: true},
+    title: {type: String, require: true},
+    shortDescription: {type: String, require: true},
+    content: {type: String, require: true},
+    blogId: {type: String, require: true},
+    blogName: {type: String, require: true},
+    createdAt: {type: String, require: true},
+})
+export const PostDbTypeModel = mongoose.model<PostDbType>('posts', PostDbTypeSchema)
