4cf3e5cb4f543be1592d67c24b1a61fc1c39d897
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:37:40 +0300

    created endpoint for like comments

--- h_two/src/routers/comments-router.ts
+++ h_two/src/routers/comments-router.ts
@@ -9,9 +9,11 @@ export const commentsRouter = Router()
 
 const commentsController = container.get(CommentsController)
 
-commentsRouter.get('/:id', commentsController.getCommentById.bind(commentsController))
+commentsRouter.get('/:id', authBearerMiddleware, commentsController.getCommentById.bind(commentsController))
 
 commentsRouter.put('/:commentId', authBearerMiddleware, contentCommentValidation, inputValidationMiddleware, commentsController.updateComment.bind(commentsController))
+
+commentsRouter.put('/:commentId/like-status', authBearerMiddleware, commentsController.updateLikeStatus.bind(commentsController))
 
 commentsRouter.delete('/:id', authBearerMiddleware, commentsController.deleteComment.bind(commentsController))
 
