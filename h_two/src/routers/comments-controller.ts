897163304450dc88085792b287f7a40411109d4e
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:37:19 +0300

    created function for like comments

--- h_two/src/routers/comments-controller.ts
+++ h_two/src/routers/comments-controller.ts
@@ -8,14 +8,15 @@ export class CommentsController {
     }
 
     async getCommentById(req: Request, res: Response) {
-        const comment = await this.commentsService.getCommentById(req.params.id)
+        const userId = req.user!._id
+        const comment = await this.commentsService.getCommentById(req.params.id, userId)
         if (!comment) {
             res.sendStatus(404)
         } else res.status(200).send(comment)
     }
 
     async updateComment(req: Request, res: Response) {
-        const newComment = await this.commentsService.updateCommentById(req.params.commentId, req.body.content, req.user!._id)
+        const newComment = await this.commentsService.updateCommentById(req.params.commentId, req.body.content, req.user!._id as string)
         if (!newComment) res.sendStatus(404)
         else if (newComment === 1) res.sendStatus(403)
         else res.sendStatus(204)
@@ -25,6 +26,16 @@ export class CommentsController {
         const newComment = await this.commentsService.deleteCommentById(req.params.id, req.user!._id)
         if (!newComment) res.sendStatus(404)
         else if (newComment === 1) res.sendStatus(403)
+        else res.sendStatus(204)
+    }
+
+    async updateLikeStatus(req: Request, res: Response){
+        const likeStatus = req.body.likeStatus
+        const userId = req.user!._id
+        //console.log(userId)
+        const commentId = req.params.commentId
+        const response = await this.commentsService.updateLikeStatus(likeStatus, userId, commentId)
+        if (response === "not found") res.sendStatus(404)
         else res.sendStatus(204)
     }
 }
