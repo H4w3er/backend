bb929bb3346b68a9e675f73ec84dd977fadeaa7f
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:36:40 +0300

    some change

--- h_two/src/middlewares/CRL-cheking.ts
+++ h_two/src/middlewares/CRL-cheking.ts
@@ -1,14 +1,14 @@
 import {Request, Response, NextFunction} from "express";
-import {requestsToApiCollection} from "../db/mongo-db";
+import {RequestsToApiModel} from "../db/requests-to-api-type-db";
 
 export const CRLChecking = async (req: Request, res: Response, next: NextFunction)=> {
     const ip = req.ip
     const url = req.originalUrl
     const date = new Date()
     if (ip!=undefined) {
-        await requestsToApiCollection.insertOne({IP: ip, URL: url, date: date});
+        await RequestsToApiModel.insertOne({IP: ip, URL: url, date: date});
         date.setSeconds(date.getSeconds() - 10)
-        const requestsCount = await requestsToApiCollection.find({IP: ip, URL: url, date: {$gte: date}}).toArray()
+        const requestsCount = await RequestsToApiModel.find({IP: ip, URL: url, date: {$gte: date}}).lean()
         if (requestsCount.length > 5) res.sendStatus(429)
         else next()
     } else res.sendStatus(429)
