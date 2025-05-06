52069aa54205897c832c085a0b84d7c04b373f3f
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:40:35 +0300

    added models to class

--- h_two/src/repositories/blogs-db-repository.ts
+++ h_two/src/repositories/blogs-db-repository.ts
@@ -1,24 +1,22 @@
-import {
-    blogCollection,
-    commentsCollection,
-    postCollection,
-    refreshTokenCollection, requestsToApiCollection,
-    userCollection
-} from "../db/mongo-db";
 import {ObjectId} from "mongodb";
 import {injectable} from "inversify";
 import {UserModel} from "../db/user-type-db";
+import {BlogDbTypeModel} from "../db/blogs-type-db";
+import {PostDbTypeModel} from "../db/posts-type-db";
+import {CommentsModel, LikerInfoModel} from "../db/comments-type-db";
+import {RefreshTokenDbModel} from "../db/refresh-Token-db";
+import {RequestsToApiModel} from "../db/requests-to-api-type-db";
 
 @injectable()
 export class BlogsDbRepository {
     async createBlog(newBlog: any) {
-        const idOfNewBlog = await blogCollection.insertOne(newBlog)
-        return idOfNewBlog.insertedId;
+        await BlogDbTypeModel.insertOne(newBlog)
+        return newBlog._id;
     }
 
     async updateBlog(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
         const objId = new ObjectId(id);
-        const result = await blogCollection.updateOne({_id: objId}, {
+        const result = await BlogDbTypeModel.updateOne({_id: objId}, {
             $set: {
                 name: name,
                 description: description,
@@ -30,23 +28,24 @@ export class BlogsDbRepository {
 
     async deleteBlog(id: string): Promise<boolean> {
         const objId = new ObjectId(id);
-        const result = await blogCollection.deleteOne({_id: objId})
+        const result = await BlogDbTypeModel.deleteOne({_id: objId})
         return result.deletedCount === 1;
     }
 
     async deleteAll(): Promise<boolean> {
-        await blogCollection.deleteMany()
-        await postCollection.deleteMany()
+        await BlogDbTypeModel.deleteMany()
+        await PostDbTypeModel.deleteMany()
         await UserModel.deleteMany()
-        await commentsCollection.deleteMany()
-        await refreshTokenCollection.deleteMany()
-        await requestsToApiCollection.deleteMany()
+        await CommentsModel.deleteMany()
+        await RefreshTokenDbModel.deleteMany()
+        await RequestsToApiModel.deleteMany()
+        await LikerInfoModel.deleteMany()
         return true;
     }
 
     async isBlog(blogId: string): Promise<boolean> {
         const id = new ObjectId(blogId)
-        const result = await blogCollection.countDocuments({_id: id})
+        const result = await BlogDbTypeModel.countDocuments({_id: id})
         return result === 1;
     }
 }
