aa3e2e0c94a54839b042e2e2931ee85f3fb3fb31
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:32:33 +0300

    some change

--- h_two/src/db/mongo-db.ts
+++ h_two/src/db/mongo-db.ts
@@ -1,9 +1,8 @@
 import {SETTINGS} from "../settings";
-import {Collection, Db, MongoClient} from "mongodb";
 import {BlogDbType} from "./blogs-type-db";
 import {PostDbType} from "./posts-type-db";
 import {UserDbTypeCommon} from "./user-type-db";
-import {refreshTokenDb} from "./refresh-Token-db";
+import {RefreshTokenDb} from "./refresh-Token-db";
 import {requestsToApi} from "./requests-to-api-type-db";
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
@@ -11,7 +10,7 @@ import {CommentsDbTypeCommon} from "./comments-type-db";
 
 dotenv.config()
 
-const dbName = 'home_works'
+const dbName = 'Data'
 const mongoURI = SETTINGS.MONGO_URI || `mongodb://0.0.0.0:27017/${dbName}`
 
 export async function runDb() {
@@ -24,19 +23,17 @@ export async function runDb() {
     }
 }
 
-
-
 // получение доступа к бд
-const client: MongoClient = new MongoClient(SETTINGS.MONGO_URI)
-export const db: Db = client.db(SETTINGS.DB_NAME);
-
-// получение доступа к коллекциям
-export const blogCollection: Collection<BlogDbType> = db.collection<BlogDbType>(SETTINGS.PATH.BLOGS)
-export const postCollection: Collection<PostDbType> = db.collection<PostDbType>(SETTINGS.PATH.POSTS)
-export const userCollection: Collection<UserDbTypeCommon> = db.collection<UserDbTypeCommon>(SETTINGS.PATH.USERS)
-export const commentsCollection: Collection<CommentsDbTypeCommon> = db.collection<CommentsDbTypeCommon>(SETTINGS.PATH.COMMENTS)
-export const refreshTokenCollection: Collection<refreshTokenDb> = db.collection<refreshTokenDb>(SETTINGS.PATH.SECURITYDEVICES)
-export const requestsToApiCollection: Collection<requestsToApi> = db.collection<requestsToApi>(SETTINGS.PATH.REQUESTSTOAPI)
+// const client: MongoClient = new MongoClient(SETTINGS.MONGO_URI)
+// export const db: Db = client.db(SETTINGS.DB_NAME);
+//
+// // получение доступа к коллекциям
+// export const blogCollection: Collection<BlogDbType> = db.collection<BlogDbType>(SETTINGS.PATH.BLOGS)
+// export const postCollection: Collection<PostDbType> = db.collection<PostDbType>(SETTINGS.PATH.POSTS)
+// export const userCollection: Collection<UserDbTypeCommon> = db.collection<UserDbTypeCommon>(SETTINGS.PATH.USERS)
+// export const commentsCollection: Collection<CommentsDbTypeCommon> = db.collection<CommentsDbTypeCommon>(SETTINGS.PATH.COMMENTS)
+// export const refreshTokenCollection: Collection<refreshTokenDb> = db.collection<refreshTokenDb>(SETTINGS.PATH.SECURITYDEVICES)
+// export const requestsToApiCollection: Collection<requestsToApi> = db.collection<requestsToApi>(SETTINGS.PATH.REQUESTSTOAPI)
 
 /*export async function runDb () {
     try {
