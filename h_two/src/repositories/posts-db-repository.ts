3c00859aee662e358109b43a9674475df86a7257
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:41:11 +0300

    added models to class

--- h_two/src/repositories/posts-db-repository.ts
+++ h_two/src/repositories/posts-db-repository.ts
@@ -1,21 +1,21 @@
-import {postCollection} from "../db/mongo-db";
 import {ObjectId} from "mongodb";
 import {injectable} from "inversify";
+import {PostDbTypeModel} from "../db/posts-type-db";
 
 @injectable()
 export class PostsDbRepository {
     async createPost(post: any){
-        const newPostId = await postCollection.insertOne(post)
-        return newPostId.insertedId;
+        await PostDbTypeModel.insertOne(post)
+        return post._id;
     }
     async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean>{
         const objId = new ObjectId(id);
-        const result = await postCollection.updateOne({_id: objId}, {$set:{title: title, shortDescription: shortDescription, content: content, blogId: blogId}})
+        const result = await PostDbTypeModel.updateOne({_id: objId}, {$set:{title: title, shortDescription: shortDescription, content: content, blogId: blogId}})
         return result.matchedCount === 1;
     }
     async deletePost(id: string): Promise<boolean>{
         const objId = new ObjectId(id);
-        const result = await postCollection.deleteOne({_id: objId})
+        const result = await PostDbTypeModel.deleteOne({_id: objId})
         return result.deletedCount === 1;
     }
 }
