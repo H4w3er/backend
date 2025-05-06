d924e4e04e1626574ab077ac716d99083848c24d
H4w3er <mr.gavrik378@list.ru>
2025-05-06 19:34:38 +0300

    added class, schema and model requests to api

--- h_two/src/db/requests-to-api-type-db.ts
+++ h_two/src/db/requests-to-api-type-db.ts
@@ -1,5 +1,18 @@
-export type requestsToApi = {
-    IP: string | string[],
-    URL: string,
-    date: Date
+import mongoose from "mongoose";
+import {ObjectId} from "mongodb";
+
+export class requestsToApi {
+    constructor(
+        public IP: string,
+        public URL: string,
+        public date: Date) {
+
+    }
 }
+
+export const requestsToApiSchema = new mongoose.Schema<requestsToApi>({
+    IP: {type: String, require: true},
+    URL: {type: String, require: true},
+    date: {type: Date, require: true},
+})
+export const RequestsToApiModel = mongoose.model<requestsToApi>('requests', requestsToApiSchema)
