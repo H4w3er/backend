c583f96b1744dbb943ca36006ffbcdfcc57f8aaf
H4w3er <mr.gavrik378@list.ru>
2025-05-09 20:40:38 +0300

    added middleware to validate like/dislike operations

--- h_two/src/middlewares/comments-validation.ts
+++ h_two/src/middlewares/comments-validation.ts
@@ -2,3 +2,7 @@ import {body, param} from "express-validator";
 
 export const contentCommentValidation = body('content').trim().isLength({min:20, max:300}).withMessage("Content should be more than 20 and less than 300");
 export const postIdCommentValidation = param('postId').trim().isLength({min:24, max:24}).withMessage("Post id should be 24 characters length");
+export const likeStatusValidation = body('likeStatus').trim().custom(async value => {
+    if (value !== 'None' || value !== 'Like' || value !== 'Dislike') throw new Error('Cant send it')
+    return true
+})
