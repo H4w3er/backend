e863591b9055988b4a51b8bafb00fca40f87140c
H4w3er <mr.gavrik378@list.ru>
2025-05-09 20:42:07 +0300

    added middleware in like-status endpoint

--- h_two/src/routers/comments-router.ts
+++ h_two/src/routers/comments-router.ts
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import {authBearerMiddleware} from "../middlewares/authBearerMiddleware";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
-import {contentCommentValidation} from "../middlewares/comments-validation";
+import {contentCommentValidation, likeStatusValidation} from "../middlewares/comments-validation";
 import {CommentsController} from "./comments-controller";
 import {container} from "../composition-root";
 
@@ -13,7 +13,7 @@ commentsRouter.get('/:id', authBearerMiddleware, commentsController.getCommentBy
 
 commentsRouter.put('/:commentId', authBearerMiddleware, contentCommentValidation, inputValidationMiddleware, commentsController.updateComment.bind(commentsController))
 
-commentsRouter.put('/:commentId/like-status', authBearerMiddleware, commentsController.updateLikeStatus.bind(commentsController))
+commentsRouter.put('/:commentId/like-status', authBearerMiddleware, likeStatusValidation, inputValidationMiddleware, commentsController.updateLikeStatus.bind(commentsController))
 
 commentsRouter.delete('/:id', authBearerMiddleware, commentsController.deleteComment.bind(commentsController))
 
