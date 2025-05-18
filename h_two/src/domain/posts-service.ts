46c6ff774d8ed5d0b3365b4ebd6f910170ba840e
H4w3er <mr.gavrik378@list.ru>
2025-05-18 16:09:05 +0300

    created function for update like status

--- h_two/src/domain/posts-service.ts
+++ h_two/src/domain/posts-service.ts
@@ -2,22 +2,37 @@ import {PostsDbRepository} from "../repositories/posts-db-repository";
 import {BlogsDbQueryRepository} from "../repositories/blogs-db-query-repository";
 import {injectable} from "inversify";
 import {ObjectId} from "mongodb";
+import {PostDbTypeModel} from "../db/posts-type-db";
+import {PostsDbQueryRepository} from "../repositories/posts-db-query-repository";
 
 @injectable()
 export class PostsService {
     constructor(protected postsDbRepository: PostsDbRepository,
-                protected blogsDbQueryRepository: BlogsDbQueryRepository) {}
+                protected blogsDbQueryRepository: BlogsDbQueryRepository,
+                protected postsDbQueryRepository: PostsDbQueryRepository) {}
 
     async createPost(title: string, shortDescription: string, content: string, blogId: string) {
-        const newPost = {
+        const newPost = new PostDbTypeModel({
             _id: new ObjectId(),
             title: title,
             shortDescription: shortDescription,
             content: content,
             blogId: blogId,
             blogName: (await this.blogsDbQueryRepository.getBlogName(blogId)),
-            createdAt: new Date().toISOString()
-        }
+            createdAt: new Date().toISOString(),
+            extendedLikesInfo: {
+                likesCount: 0,
+                dislikesCount: 0,
+                myStatus: 'None',
+                newestLikes: [
+                    {
+                        addedAt: "-",
+                        userId: "-",
+                        login: "-"
+                    }
+                ]
+            }
+        })
         return this.postsDbRepository.createPost(newPost)
     }
 
@@ -28,5 +43,11 @@ export class PostsService {
     async deletePost(id: string): Promise<boolean> {
         return this.postsDbRepository.deletePost(id)
     }
-
+    async updatedLikeStatus(likeStatus: string, userId: string, postId: string){
+        const post = await this.postsDbQueryRepository.findPostById(userId)
+        if (!post) return 'not found'
+        const oldStatus = await this.postsDbQueryRepository.getLikeStatus(userId, postId)
+        if (!oldStatus) return 'not found'
+        return 
+    }
 }
