eda9400f1ed7e6cf08c96c63105f69c4eaa41e6f
H4w3er <mr.gavrik378@list.ru>
2025-05-18 16:08:40 +0300

    created schema and model for likes to posts

--- h_two/src/db/posts-type-db.ts
+++ h_two/src/db/posts-type-db.ts
@@ -9,9 +9,35 @@ export class PostDbType {
         public content: string,
         public blogId: string,
         public blogName: string,
-        public createdAt: string
-    ) {
+        public createdAt: string,
+        public extendedLikesInfo: {
+            likesCount: number,
+            dislikesCount: number,
+            myStatus: string
+            newestLikes: [
+                {
+                    addedAt: string,
+                    userId: string,
+                    login: string
+                }
+            ]
+        }) {
+    }
+}
 
+export class LikerPostInfo {
+    constructor(
+        public likerId: string,
+        public status: string,
+        public postId: string,
+        public newestLikes: [
+            {
+                addedAt: string,
+                userId: string,
+                login: string
+            }
+        ]
+    ) {
     }
 }
 
@@ -23,5 +49,32 @@ export const PostDbTypeSchema = new mongoose.Schema<PostDbType>({
     blogId: {type: String, require: true},
     blogName: {type: String, require: true},
     createdAt: {type: String, require: true},
+    extendedLikesInfo: {
+        likesCount: Number,
+        dislikesCount: Number,
+        myStatus: {type: String, enum: ['None', 'Like', 'Dislike']},
+        newestLikes: [
+            {
+                addedAt: {type: String, default: "-"},
+                userId: {type: String, default: "-"},
+                login: {type: String, default: "-"},
+            }
+        ]
+    }
 })
+
+export const LikerPostInfoSchema = new mongoose.Schema<LikerPostInfo>({
+    likerId: {type: String, require: true},
+    status: {type: String, require: true},
+    postId: {type: String, require: true},
+    newestLikes: [
+        {
+            addedAt: {type: String},
+            userId: {type: String},
+            login: {type: String},
+        }
+    ]
+})
+
 export const PostDbTypeModel = mongoose.model<PostDbType>('posts', PostDbTypeSchema)
+export const LikerPostInfoModel = mongoose.model<LikerPostInfo>('likesToPost', LikerPostInfoSchema)
