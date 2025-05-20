7dc0084814c033df1b02fa8ed251fce8d693931e
H4w3er <mr.gavrik378@list.ru>
2025-05-20 17:30:29 +0300

    some changes done to create like posts functionality

--- h_two/src/routers/posts-controller.ts
+++ h_two/src/routers/posts-controller.ts
@@ -3,12 +3,14 @@ import {PostsService} from "../domain/posts-service";
 import {PostsDbQueryRepository} from "../repositories/posts-db-query-repository";
 import {CommentsService} from "../domain/comments-service";
 import {Request, Response} from "express";
+import {JwtService} from "../application/jwt-service";
 
 @injectable()
 export class PostsController {
     constructor(protected postsService: PostsService,
                 protected postDbQueryRepository: PostsDbQueryRepository,
-                protected commentsService: CommentsService) {
+                protected commentsService: CommentsService,
+                protected jwtService: JwtService) {
     }
 
     async getPosts(req: Request, res: Response) {
@@ -21,14 +23,26 @@ export class PostsController {
     }
 
     async createPost(req: Request, res: Response) {
+        const authToken = req.headers.authorization as string
         const newPostId = await this.postsService.createPost(req.body.title, req.body.shortDescription,
-            req.body.content, req.body.blogId)
-        const newPost = await this.postDbQueryRepository.findPostById(newPostId.toString())
-        res.status(201).send(newPost);
+            req.body.content, req.body.blogId, authToken)
+        if (newPostId == 'not found') res.sendStatus(401)
+        else {
+            const newPost = await this.postDbQueryRepository.findPostById(newPostId.toString(), 'nothing')
+            res.status(201).send(newPost)
+        }
     }
 
     async getPostById(req: Request, res: Response) {
-        let post = await this.postDbQueryRepository.findPostById(req.params.id)
+        const authToken = req.headers.authorization as string
+        let user = null
+        let post = null
+        if (authToken) {
+            user = await this.jwtService.getIdFromToken(authToken.split(' ')[1] as string)
+        }
+        console.log(user)
+        if (!user.userId) post = await this.postDbQueryRepository.findPostById(req.params.id, 'nothing')
+        else  post = await this.postDbQueryRepository.findPostById(req.params.id, user.userId)
         if (post) {
             res.status(200).send(post)
         } else res.sendStatus(404);
@@ -50,7 +64,7 @@ export class PostsController {
     }
 
     async createCommentForPost(req: Request, res: Response) {
-        let post = await this.postDbQueryRepository.findPostById(req.params.postId)
+        let post = await this.postDbQueryRepository.findPostById(req.params.postId, 'nothing')
         if (!post) {
             res.sendStatus(404);
         } else {
@@ -60,7 +74,7 @@ export class PostsController {
     }
 
     async getCommentsForPost(req: Request, res: Response) {
-        let post = await this.postDbQueryRepository.findPostById(req.params.postId)
+        let post = await this.postDbQueryRepository.findPostById(req.params.postId, 'nothing')
         const authToken = req.headers.authorization as string
         if (!post) {
             res.sendStatus(404);
@@ -74,11 +88,12 @@ export class PostsController {
         }
     }
 
-    async updatedLikeStatus(req: Request, res: Response){
+    async updatedLikeStatus(req: Request, res: Response) {
         const likeStatus = req.body.likeStatus
         const userId = req.user!._id
         const postId = req.params.id
         const response = await this.postsService.updatedLikeStatus(likeStatus, userId, postId)
-
+        if (response === "not found") res.sendStatus(404)
+        else res.sendStatus(204)
     }
 }
