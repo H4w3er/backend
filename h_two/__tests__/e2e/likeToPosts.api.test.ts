716a0cb29366f4bd3ef5e7fc5e6e79c3df42f126
H4w3er <mr.gavrik378@list.ru>
2025-05-22 20:24:58 +0300

    created tests for likesToPosts

---
+++ h_two/__tests__/e2e/likeToPosts.api.test.ts
@@ -0,0 +1,96 @@
+import request from "supertest";
+import {app} from "../../src";
+
+describe('Likes to posts tests', () => {
+    beforeAll(async () => {
+        await request(app).delete('/testing/all-data')
+    })
+    afterAll(async () => {
+        await request(app).delete('/testing/all-data')
+    })
+    describe('likes to posts', () => {
+        it('should create two new users', async () => {
+            const authorizationToken = 'Basic admin:qwerty'
+            const firstUser = await request(app)
+                .post('/users')
+                .set('Authorization', authorizationToken)
+                .send({
+                    "login": "firstUser",
+                    "email": "email1221artem@mail.com",
+                    "password": "string"
+                })
+            const secondUser = await request(app)
+                .post('/users')
+                .set('Authorization', authorizationToken)
+                .send({
+                    "login": "secUser",
+                    "email": "superEmail@mail.com",
+                    "password": "string"
+                })
+            expect(firstUser.status).toBe(201)
+            expect(secondUser.status).toBe(201)
+        })
+
+        it('should login two users, create blog and posts than likes', async () => {
+            const firstUserLogin = await request(app)
+                .post('/auth/login')
+                .send({
+                    "loginOrEmail": "firstUser",
+                    "password": "string"
+                })
+            const secondUserLogin = await request(app)
+                .post('/auth/login')
+                .send({
+                    "loginOrEmail": "secUser",
+                    "password": "string"
+                })
+            expect(firstUserLogin.status).toBe(200)
+            expect(secondUserLogin.status).toBe(200)
+
+            const authorizationToken = 'Basic admin:qwerty'
+            const mainBlogCreation = await request(app)
+                .post('/blogs')
+                .set('Authorization', authorizationToken)
+                .send({
+                    "name": "first",
+                    "description": "blog",
+                    "websiteUrl": "https://wuj5_R1TaqiUYYdzeDMXtdS7Qm_ga-bppGIlWSzfOuY9lJO20Nh33OuFCEWnDw5U4O2_3jEfqDFZSKkX4HfD7Eo.v0xQ"
+                })
+            const firstPostCreation = await request(app)
+                .post('/posts')
+                .set('Authorization', authorizationToken)
+                .send({
+                    "title": "first",
+                    "shortDescription": "my first post to this blog",
+                    "content": "there is no content bro",
+                    "blogId": mainBlogCreation.body.id
+                })
+            expect(mainBlogCreation.status).toBe(201)
+            expect(firstPostCreation.status).toBe(201)
+
+            const accessTokenFirst = firstUserLogin.body.accessToken
+            const accessTokenSecond = secondUserLogin.body.accessToken
+            const likePostByFirstUser = await request(app)
+                .put(`/posts/${firstPostCreation.body.id}/like-status`)
+                .set('Authorization', 'Bearer ' + accessTokenFirst)
+                .send({
+                    "likeStatus": "Like"
+                })
+            expect(likePostByFirstUser.status).toBe(204)
+
+            const likePostBySecUser = await request(app)
+                .put(`/posts/${firstPostCreation.body.id}/like-status`)
+                .set('Authorization', 'Bearer ' + accessTokenSecond)
+                .send({
+                    "likeStatus": "Like"
+                })
+            expect(likePostBySecUser.status).toBe(204)
+
+
+            const getAllPostsByFirstUser = await request(app)
+                .get('/posts')
+                .set('Authorization', 'Bearer ' + accessTokenSecond)
+            console.log(getAllPostsByFirstUser.body.items[0].extendedLikesInfo.newestLikes)
+        })
+    })
+})
