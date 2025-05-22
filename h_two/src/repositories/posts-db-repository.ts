d246c66bb4c5fc61ffa8fd49a10ca3e46bd30455
H4w3er <mr.gavrik378@list.ru>
2025-05-22 20:29:09 +0300

    added requests to LastLikesModel for detecting last likes

--- h_two/src/repositories/posts-db-repository.ts
+++ h_two/src/repositories/posts-db-repository.ts
@@ -1,38 +1,67 @@
 import {ObjectId} from "mongodb";
 import {injectable} from "inversify";
-import {LikerPostInfoModel, PostDbTypeModel} from "../db/posts-type-db";
+import {LastLikersModel, LikerPostInfoModel, PostDbTypeModel} from "../db/posts-type-db";
 
 @injectable()
 export class PostsDbRepository {
-    async createPost(post: any){
+    async createPost(post: any) {
         await post.save()
-        await LikerPostInfoModel.create({likerId: post.userId.toString(), status: 'None', postId: post._id.toString()})
         return post._id;
     }
-    async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean>{
+
+    async updatePost(id: string, title: string, shortDescription: string, content: string, blogId: string): Promise<boolean> {
         const objId = new ObjectId(id);
-        const result = await PostDbTypeModel.updateOne({_id: objId}, {$set:{title: title, shortDescription: shortDescription, content: content, blogId: blogId}})
+        const result = await PostDbTypeModel.updateOne({_id: objId}, {
+            $set: {
+                title: title,
+                shortDescription: shortDescription,
+                content: content,
+                blogId: blogId
+            }
+        })
         return result.matchedCount === 1;
     }
-    async deletePost(id: string): Promise<boolean>{
+
+    async deletePost(id: string): Promise<boolean> {
         const objId = new ObjectId(id);
         const result = await PostDbTypeModel.deleteOne({_id: objId})
         return result.deletedCount === 1;
     }
-    async updateLikeStatus(newLikeStatus: string, userId: string, postId: string, oldStatus: string){
+
+    async updateLikeStatus(newLikeStatus: string, userId: string, postId: string, oldStatus: string, userLogin: string) {
         const postIdObj = new ObjectId(postId)
         await LikerPostInfoModel.updateOne({likerId: userId, postId: postId}, {$set: {status: newLikeStatus}})
-        if (newLikeStatus === 'None' && oldStatus === 'Like') await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: { "extendedLikesInfo.likesCount": -1}})
-        if (newLikeStatus === 'None' && oldStatus === 'Dislike') await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: { "extendedLikesInfo.dislikesCount": -1}})
-        if (newLikeStatus === 'Like' && oldStatus === 'Dislike') {
-            await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: { "extendedLikesInfo.likesCount": 1}})
-            await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.dislikesCount": -1}})
+        if (newLikeStatus === 'None' && oldStatus === 'Like') {
+            await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.likesCount": -1}})
+            await LastLikersModel.deleteOne({userId: userId})
         }
-        if (newLikeStatus === 'Like' && oldStatus === 'None') await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: { "extendedLikesInfo.likesCount": 1}})
-        if (newLikeStatus === 'Dislike' && oldStatus === 'None') await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: { "extendedLikesInfo.dislikesCount": 1}})
-        if (newLikeStatus === 'Dislike' && oldStatus === 'Like'){
+        if (newLikeStatus === 'None' && oldStatus === 'Dislike') await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.dislikesCount": -1}})
+        if (newLikeStatus === 'Like' && oldStatus === 'Dislike') {
+            await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.likesCount": 1}})
+            await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.dislikesCount": -1}})
+            await LastLikersModel.create({
+                newestLikes: [{
+                    addedAt: new Date().toISOString(),
+                    userId: userId,
+                    login: userLogin
+                }]
+            })
+        }
+        if (newLikeStatus === 'Like' && oldStatus === 'None') {
+            await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.likesCount": 1}})
+            await LastLikersModel.create({
+                newestLikes: [{
+                    addedAt: new Date().toISOString(),
+                    userId: userId,
+                    login: userLogin
+                }]
+            })
+        }
+        if (newLikeStatus === 'Dislike' && oldStatus === 'None') await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.dislikesCount": 1}})
+        if (newLikeStatus === 'Dislike' && oldStatus === 'Like') {
             await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.likesCount": -1}})
             await PostDbTypeModel.updateOne({_id: postIdObj}, {$inc: {"extendedLikesInfo.dislikesCount": +1}})
+            await LastLikersModel.deleteOne({userId: userId})
         }
     }
 }
